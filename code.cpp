8eaa5383d0eaa55a1250208b43beb1c815088ff4
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-03-16 16:54:52 +0000

    Глава 3. Упражнение 9.

--- code.cpp
+++ code.cpp
@@ -5,13 +5,22 @@ using namespace std;
 
 //c++ -o code code.cpp
 int main () {
+	string a;
 
-	cout << "Введите число для проверки чётности: ";
-	int a;
-	cin >> a;
-	
-	if ( a%2 == 0 ) cout << "Число " << a << " чётное :)\n";
-		else cout << "Число " << a << " не чётное :(\n";
+	cout << "Вводите число: ";
+	while (true)
+		{
+		cin >> a;
+		if (a == "1") cout << "Один\n";
+			else if (a == "2") cout << "Два\n";
+				else if (a == "3") cout << "Три\n";
+					else if (a == "4") cout << "Четыре\n";
+						else if (a == "Один") cout << "1\n";
+							else if (a == "Два") cout << "2\n";
+								else if (a == "Три") cout << "3\n";
+									else if (a == "Четыре") cout << "4\n";
+										else cout << "Не знаю такого числа :(\n";
+		}
 
 return 0;
 }
