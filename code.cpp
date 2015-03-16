4c6085ea878c5ce65e2dc0e6521695bfc44a3c2c
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-03-16 16:43:51 +0000

    Глава 3 Упр. 7 Сравнение строк типа String.

--- code.cpp
+++ code.cpp
@@ -1,42 +1,24 @@
 #include <iostream>
 #include <fstream>
+//#include <string.h>
 
 using namespace std;
 
 //c++ -o code code.cpp
+//*Steinbeck* *Heminway* *Fitzgerald*
 int main () {
-	cout << "Введите количество целых чисел, которые будите вводить: ";
-	int n;
-	cin >> n;
-	cout << "Введите три целых числа через пробел: ";
-	int A[20],i;
-	for (i=0; i<n; i++)
-		cin >> A[i];
 
-					for (i=0; i<n; i++)
-						cout << A[i] << ' ';
-					cout << endl;
-
-	int buf;
-	bool k=false;
-	i=0;
-	while (true) {
-		if (A[i] > A[i+1]) {
-			buf=A[i];
-			A[i]=A[i+1];
-			A[i+1]=buf;
-			k=true;
-			}
-		i++;
-		if (i==n-1)
-			if (k) { i=0; k=false; }
-				else {
-					for (int j=0; j<n; j++)
-						cout << A[j] << ' ';
-					cout << endl;
-					return 0;
-					}
-		}
+	cout << "Введите через пробел три значения: ";
+	string a,b,c;
+	cin >> a >> b >> c;
+	
+	if ( a < b && b<c) cout << a << ", " << b << ", " << c <<endl;
+	if ( a < c && c<b) cout << a << ", " << c << ", " << b <<endl;
+	if ( c < b && b<a) cout << c << ", " << b << ", " << a <<endl;
+	if ( c < a && a<b) cout << c << ", " << a << ", " << b <<endl;
+	if ( b < c && c<a) cout << b << ", " << c << ", " << a <<endl;
+	if ( b < a && a<c) cout << b << ", " << a << ", " << c <<endl;
+	
 return 0;
 }
 
