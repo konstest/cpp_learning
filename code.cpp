6a930c69c6087954c13ad6713e8396993c20c819
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-03-16 14:15:50 +0000

    Глава 3. Упр 6: Упорядочивание целых чисел.

--- code.cpp
+++ code.cpp
@@ -5,15 +5,38 @@ using namespace std;
 
 //c++ -o code code.cpp
 int main () {
-	cout << "Введите через пробел val1 и val2: ";
-	double val1,val2;
-	cin >> val1 >> val2;
-	if (val1 == val2) cout << "val1 = val2\n";
-		else if (val1 > val2) cout << "val1 > val2\n";
-			else cout << "val1 < val2\n";
-	cout << "val1 + val2 = " << val1+val2 << endl;
-	cout << "val1 - val2 = " << val1-val2 << endl;
-	cout << "val1 * val2 = " << val1*val2 << endl;
-	cout << "val1 / val2 = " << val1/val2 << endl;
+	cout << "Введите количество целых чисел, которые будите вводить: ";
+	int n;
+	cin >> n;
+	cout << "Введите три целых числа через пробел: ";
+	int A[20],i;
+	for (i=0; i<n; i++)
+		cin >> A[i];
+
+					for (i=0; i<n; i++)
+						cout << A[i] << ' ';
+					cout << endl;
+
+	int buf;
+	bool k=false;
+	i=0;
+	while (true) {
+		if (A[i] > A[i+1]) {
+			buf=A[i];
+			A[i]=A[i+1];
+			A[i+1]=buf;
+			k=true;
+			}
+		i++;
+		if (i==n-1)
+			if (k) { i=0; k=false; }
+				else {
+					for (int j=0; j<n; j++)
+						cout << A[j] << ' ';
+					cout << endl;
+					return 0;
+					}
+		}
 return 0;
 }
+
