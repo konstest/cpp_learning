31f460881899f09e34074ece93617361d2fa4cb3
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-03-27 15:52:11 +0000

    Глава 4. Стр 156. Упр. 6

--- code.cpp
+++ code.cpp
@@ -7,26 +7,33 @@
 using namespace std;
 
 int main () {
-	double arg1, arg2;
-	char op;
-	cout << "Введите два числа и оператор (+-/*):\n";
-	while (cin>>arg1>>arg2>>op)
-		switch (op)
-			{
-			case '+':
-				cout << "Сумма " << arg1 << " и " << arg2 << " равна " << arg1 + arg2 << endl;
-				break;
-			case '-':
-				cout << "Разница " << arg1 << " и " << arg2 << " равна " << arg1 - arg2 << endl;
-				break;
-			case '*':
-				cout << "Произведение " << arg1 << " и " << arg2 << " равно " << arg1 * arg2 << endl;
-				break;
-			case '/':
-				cout << "Частное " << arg1 << " и " << arg2 << " равно " << arg1 / arg2 << endl;
-				break;
-			}
-	
+	string word;
+//	vector<string> number;
+	vector<string> number(10);
+	number[0] = "zero";
+	number[1] = "one";
+	number[2] = "two";
+	number[3] = "three";
+	number[4] = "four";
+	number[5] = "five";
+	number[6] = "six";
+	number[7] = "seven";
+	number[8] = "eight";
+	number[9] = "nine";
+/*	cout << "Введите цифры прописью:\n";
+	for (int i = 0; i < 10; i++)
+		{
+		cout << i << ": ";
+		cin>>word;
+		number.push_back(word);
+		}
+*/
+	string num;
+	cout << "Введите цифру прописью:\n";
+	while (cin>>num)
+		for (int i = 0; i < 10; i++)
+			if (number[i] == num) cout << i <<endl;
+
 	return 0;
 }
 
