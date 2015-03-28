3f6e0ff4d100219a84fa0f2c2e08cc2ef0d8a7c6
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-03-28 12:22:03 +0000

    Глава 4. Стр 158. Упр. 13-14. Нахождение вектора простых чисел методом Решето Эратосфена.

--- code.cpp
+++ code.cpp
@@ -6,30 +6,62 @@
 
 using namespace std;
 /*
-	Глава 4 Упражнение 11. Нахождение вектора простых чисел от 1 до 100
+						//https://ru.wikipedia.org/wiki/Решето_Эратосфена
+	Глава 4 Упражнение 11. Нахождение вектора простых чисел от 1 до 100 методом ЭРАТОСФЕНА.
 	Просто́е число́ — это натуральное (целое положительное) число, имеющее ровно два различных натуральных делителя. Другими словами, число p простое, если оно больше 1 и делится только на 1 и на p.
 */
-
 int main () {
-	int value;
-	vector<int> primes;
-	bool not_prime;
-	primes.push_back(1);
+	int value,i;
 	cout << "Введите максимум, до которого нужно расчитать вектор простых чисел: ";
 	cin>>value;
-	for (int i = 2; i <= value; i++)
+	//Создаём вектор размерностью value, в котором пока все значения равны "1"
+	vector<int> primes;
+	for (i = 0; i < value; i++)
+		primes.push_back(1);
+
+	//Оставляем в инициализированном выше векторе единицы только на позициях простых чисел
+	int p = 2, step=0;
+	bool deleting=true;
+	while (deleting)
 		{
-		not_prime = false;
-		for (int j = 2; j < i; j++)
-			if (i%j == 0) //Если число делится число меньше себя без остатка, то
+		deleting=false;
+		if (step < 2)
+			{
+			for (i = 2*p; i < value; i+=p )
+				if (primes[i] == 1)
+					{
+					primes[i] = 0;
+					deleting = true;
+					}
+			}
+		else //С третьего шага начинаем удалять составныечисла числа начиная с позиции p*p
+				// т.к. до этой позиции они уже удалены
+			{
+			for (i = p*p; i < value; i+=p )
+				if (primes[i] == 1)
+					{
+					primes[i] = 0;
+					deleting = true;
+					}
+			}
+				
+		for (i = p + 1; i < value; i++)
+			if (primes[i] == 1 )
 				{
-				not_prime = true;  // это не простое число
+				p = i;
 				break;
 				}
-		if (!not_prime) primes.push_back(i);
+		step++;
 		}
-	for (int i=0; i < primes.size(); i++)
-		cout << i+1 << ": " << primes[i] << endl;
+	//Выводим вектор на экран
+	int j = 0;
+	for (i=1; i < primes.size(); i++)
+		if (primes[i] == 1)
+			{
+			cout << j+1 << ": " << i << endl;
+			j++;
+			}
+	cout << "Количество выполненых итераций: " << step << endl;
 	return 0;
 }
 
