e98accb52c5ba07510ac377f4e72b34b7184a998
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-03-30 13:24:36 +0000

    Глава 4. Стр 158. Упр 16-17. Нахождение минимального, максимального и моды списка чисел.

--- code.cpp
+++ code.cpp
@@ -6,32 +6,56 @@
 
 using namespace std;
 /*
-	Глава 4 Упражнение 15. Нахождение N-ого количества простых чисел
-	Просто́е число́ — это натуральное (целое положительное) число, имеющее ровно два различных натуральных делителя. Другими словами, число p простое, если оно больше 1 и делится только на 1 и на p.
+	Глава 4 Упражнение 16. Напишите программу определяющую моду набора положительных чисел.
+	Мода — это число в последовательности чисел повторяющееся наибольшее количество раз.
 */
 
 int main () {
-	int value, i;
-	vector<int> primes;
-	bool not_prime;
-	primes.push_back(1);
-	cout << "Введите количество простых чисел которое нужно расчитать: ";
-	cin>>value;
-	i = 2;
-	while (primes.size() < value)
+	int i,j, value, moda=-1, max=0, count=0, min_v, max_v;
+	vector<int> v;
+	bool repeat_moda = false;
+	
+	//INPUT
+	cout << "Введите последовательность положительных чисел:\n";
+	while (cin>>value)
 		{
-		not_prime = false;
-		for (int j = 2; j < i; j++)
-			if (i%j == 0) //Если число делится число меньше себя без остатка, то
-				{
-				not_prime = true;  // это не простое число
-				break;
-				}
-		if (!not_prime) primes.push_back(i);
-		i++;
+		if (value < 0)
+			{
+			cout << "Ошибка!!! Вектор должен состоять из положительных чисел!\n";
+			return 1;
+			}
+		v.push_back(value);
 		}
-	for (int i=0; i < primes.size(); i++)
-		cout << i+1 << ": " << primes[i] << endl;
+		
+	//COMPUTING
+	min_v = v[0];
+	max_v = v[0];
+	while (v.size() > 0)
+		{
+		if (max_v < v[0]) max_v = v[0];
+		if (min_v > v[0]) min_v = v[0];
+		count = 0;
+		for (j = 0; j < v.size(); j++)
+			if (v[0] == v[j])
+				count++;
+		if (max == count)
+			repeat_moda = true;			
+		if (max < count && count > 1)
+			{
+			moda = v[0];
+			max = count;
+			repeat_moda = false;
+			}
+		v.erase(v.begin());
+		}
+
+	//OUTPUT
+	cout << "Наименьшее число: " << min_v << endl;
+	cout << "Наибольшее число: " << max_v << endl;
+	if (!repeat_moda && moda != -1)
+		cout << "Модой является - " << moda << " повторяется " << max << " раз(а).\n";
+	else 
+		cout << "В данном списке чисел нет МОДЫ.\n";
 	return 0;
 }
 
