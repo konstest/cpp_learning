046a75fbcaf7ba5a64cf31419d5f642aeee63626
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-04-05 13:58:06 +0000

    Глава 5. Упражнение 2 - 5. Перевод температуры из Целься в Кельвины и наоборот.

--- code.cpp
+++ code.cpp
@@ -6,61 +6,29 @@
 
 using namespace std;
 /*
-	Глава 4 Упражнение 19 - 21.
+	Глава 5 Упражнение 2. Перевод температуры из Целься в Кельвины и наоборот.
 */
 
-int main () {
-	bool search;
-	string name;
-	vector<string> names(14);
-	names[0] = "Konstantin";
-	names[1] = "Margarita";
-	names[2] = "Irina";
-	names[3] = "Sergey";
-	names[4] = "Vadik";
-	names[5] = "test";
-	names[6] = "kos";
-	names[7] = "Konstasdefntin";
-	names[8] = "Marg34arita";
-	names[9] = "Irdasfna";
-	names[10] = "Se13rgey";
-	names[11] = "Vfdadik";
-	names[12] = "tfest";
-	names[13] = "kdfsos";
-	int score;
-	vector<int> scores(14);
-	scores[0] = 7;
-	scores[1] = 6;
-	scores[2] = 5;
-	scores[3] = 4;
-	scores[4] = 3;
-	scores[5] = 2;
-	scores[6] = 1;
-	scores[7] = 7;
-	scores[8] = 6;
-	scores[9] = 5;
-	scores[10] = 4;
-	scores[11] = 3;
-	scores[12] = 2;
-	scores[13] = 1;
-
-	//INPUT
-	cout << "Для пойска введите количество баллов и нажмите [ENTER]:\n";
-	while (cin >> score)
+double ctok(double c)
+//Преобразует шкалу Кельвина в шкалу Цельсия
+{
+	if (c < 0)
 		{
-		search = false;
-		for (int i = 0; i < names.size(); i++)
-			{
-			if (score == scores[i])
-				{
-				cout << "(" << names[i] << "," << scores[i] << ")\n";
-				search = true;
-				}
-			}
-		if (!search)  cout << "Score not found!\n";
+		cerr << "ОШИБКА! Введённая температура ниже абсолютного нуля!\n";
+ 		exit(1);
 		}
+	double k = c - 273.15;
+	return k;
+}
+
+int main () {
+	double c = 0;				// объявляем переменную для ввода
+	//INPUT
+	cin >> c;						// вводим температуру в переменную ввода
 	//COMPUTING
-	
+	double k = ctok(c);	// преобразуем температуру
+	//OUTPUT
+	cout << k << endl;
 
 	return 0;
 }
