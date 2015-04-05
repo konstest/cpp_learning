4ff8c6aae78d5ebafb162ca545660b85c17002f3
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-04-05 16:43:15 +0000

    Глава 5. Упражнение 7. Решение квадратичного уравнения вида ax^2 + bx + c = 0, с обработкой исключения.

--- code.cpp
+++ code.cpp
@@ -1,37 +1,57 @@
 //c++ -o code code.cpp
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <cmath>
 #include <algorithm>
 
 using namespace std;
 /*
-	Глава 5 Упражнение 2 - 5. Перевод температуры из Целься в Кельвины и наоборот.
+	Глава 5. Упражнение 7. Решение квадратичного уравнения вида ax^2 + bx + c = 0, с обработкой исключения.
 */
 
-double ctok(double c)
-//Преобразует шкалу Кельвина в шкалу Цельсия
+//Тип, созданный специально для сообщения о отрицательном дискриминанте
+class negative_root { };
+
+int abc(double A, double B, double C)
 {
-	if (c < -273.15)
+	double D, x1, x2;
+	cout << "Вычисляем решение уравнения " << A << "*(x^2) + " << B << "*x + " << C <<" = 0\n";
+	D = B*B - 4*A*C;
+	if (D > 0)
 		{
-		cerr << "ОШИБКА! Введённая температура ниже абсолютного нуля!\n";
- 		exit(1);
+		x1 = (-B + sqrt(D))/(2*A);
+		x2 = (-B - sqrt(D))/(2*A);	
+		//OUTPUT
+		cout << "Корни квадратного уравнения равны:\n";
+		cout << "x1 = " << x1 << endl;
+		cout << "x2 = " << x2 << endl;
 		}
-	double f = (9.0/5.0)*c + 32;
-	return f;
+	else	// Генерируем исключение
+		{
+		throw negative_root();
+		cout << "D = " << D << endl;
+		}
+return 0;
 }
 
 int main () {
-	double c = 0;				// объявляем переменную для ввода
+try {
+	double A, B, C, D;
 	//INPUT
-	cout << "Введите температуру по Цельсию: ";
-	cin >> c;						// вводим температуру в переменную ввода
-	//COMPUTING
-	double k = ctok(c);	// преобразуем температуру
-	//OUTPUT
-	cout << "По Фаренгейту: " 	<< k << endl;
+	cout << "Решение квадратичного уравнения вида A*(x^2) + B*x + C = 0\n";
+	cout << "введите А: ";
+	cin >> A;
+	cout << "введите B: ";
+	cin >> B;
+	cout << "введите C: ";
+	cin >> C;
 
+	//COMPUTING
+	abc(A,B,C);
+}
+catch (negative_root) {
+	cerr << "ОШИБКА! Отрицательный дискриминант!\n";
+}
 	return 0;
 }
-
 
