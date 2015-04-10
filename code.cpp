f3112df4433953a9be1e3b712030c601f7c9a1d0
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-04-10 03:07:34 +0000

    try/catch-исключения. Глава 5. Упражнение 8-9. Ввод вектора V чисел и нахождение суммы первых N элементов. И создание вектора R разностей между соседними N-1 элементами вектора V.

--- code.cpp
+++ code.cpp
@@ -1,57 +1,101 @@
-//c++ -o code code.cpp
-#include <iostream>
-#include <fstream>
-#include <cmath>
-#include <algorithm>
+#include <iostream>       // std::cerr
+#include <stdexcept>      // std::out_of_range
+#include <vector>         // std::vector
+#include <string>
 
 using namespace std;
 /*
-	Глава 5. Упражнение 7. Решение квадратичного уравнения вида ax^2 + bx + c = 0, с обработкой исключения.
+	c++ -o code code.cpp -std=c++11
+	try/catch-исключения. Глава 5. Упражнение 8-9. Ввод вектора V чисел и нахождение
+	суммы первых N элементов. И создание вектора R разностей между соседними N-1
+	элементами вектора V.
 */
 
-//Тип, созданный специально для сообщения о отрицательном дискриминанте
-class negative_root { };
-
-int abc(double A, double B, double C)
+int input(vector<double> &v)
 {
-	double D, x1, x2;
-	cout << "Вычисляем решение уравнения " << A << "*(x^2) + " << B << "*x + " << C <<" = 0\n";
-	D = B*B - 4*A*C;
-	if (D > 0)
-		{
-		x1 = (-B + sqrt(D))/(2*A);
-		x2 = (-B - sqrt(D))/(2*A);	
-		//OUTPUT
-		cout << "Корни квадратного уравнения равны:\n";
-		cout << "x1 = " << x1 << endl;
-		cout << "x2 = " << x2 << endl;
-		}
-	else	// Генерируем исключение
-		{
-		throw negative_root();
-		cout << "D = " << D << endl;
-		}
-return 0;
+//Вводим элементы вектора
+
+	string element;
+	while (cin >> element)
+		if ( element != "|" )
+			v.push_back(stod(element));
+		else
+			break;
+
+	return 0;
 }
 
+int summ(vector<double> v, int N)
+{
+//вычисляем сумму первых N эл-ов вектора.
+	double summ = 0;
+	if (N > 1)
+		{
+		if (N > v.size()) throw runtime_error( "N = "+ to_string(N) +" больше чем "+ to_string(v.size()) + " элемента(ов) вектора." );
+		cout << "Сумма первых " << N << " чисел:";
+		for (int i = 0; i < N; i++)
+			{
+			if (i < N-1) cout << " " << v[i];
+				else	cout << " и " << v[i];
+			summ += v[i];
+			}
+		cout << " равна " << summ << ".\n";
+		}
+	else
+		throw runtime_error("Количество чисел(N) должно быть больше 1 и меньше "+to_string(v.size()) );
+
+	return 0;
+}
+
+vector<double> residual (vector<double> v, int N)
+{//Нахождение разности рядом стоящих элементов с 1-ого по N-ый.
+//т.е. vec: 1 3 2 5 6 7, N = 4, то разность: 1-3=-2, 3-2=1, 2-5=-3
+	vector<double> r;
+	for (int i=0; i<N-1; i++)
+		r.push_back(v[i]-v[i+1]);
+	
+	return r;
+}
+
+
+//--------------------------------------------------------------------------------
 int main () {
+
 try {
-	double A, B, C, D;
+	vector<double> v,r;
+	unsigned int N;
+//	cout << sizeof() << endl;
 	//INPUT
-	cout << "Решение квадратичного уравнения вида A*(x^2) + B*x + C = 0\n";
-	cout << "введите А: ";
-	cin >> A;
-	cout << "введите B: ";
-	cin >> B;
-	cout << "введите C: ";
-	cin >> C;
+	cout << "Вводите элементы вектора, | - означает конец ввода:\n";
+	input(v);
+
+	cout << "Пожайлуста, введите количество чисел(N), которые хотите просумировать (начиная с первого):\n";
+	if ( !(cin >> N) )
+		throw runtime_error( "Количество чисел(N) - есть число типа(int) от 2 до " + to_string( v.size() ) );
 
 	//COMPUTING
-	abc(A,B,C);
+	summ(v,N);
+	r = residual(v,N);
+	cout << "Вектор N-1 разностей между соседними величинами: ";
+	for (int i=0; i<r.size(); i++)
+		cout << r[i] << " ";
+	cout << endl;
 }
-catch (negative_root) {
-	cerr << "ОШИБКА! Отрицательный дискриминант!\n";
+
+catch (out_of_range) {
+//  cerr << "Значения элементов вектора должны входить в предел от -2147483647 до 2147483647" << '\n'; 
+  cerr << "Значения элементов вектора должны входить в предел типа double" << endl; 
+  return 1;
 }
-	return 0;
+catch (invalid_argument) {
+  cerr << "Елементы вектора - есть целочисленный(int) тип!" << '\n'; 
+  return 2;
+}
+catch (exception& e) {
+  cerr << "Ошибка: " << e.what() << '\n'; 
+  return 3;
+}
+
+return 0;
 }
 
