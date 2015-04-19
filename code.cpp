2d1406f8d6d6a26173badeaa9058ab7dee1a48da
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-04-19 15:54:29 +0000

    Глава 6. Упражнение 4. Разные способы использования конструкторов при инициализации объектов класса

--- code.cpp
+++ code.cpp
@@ -1,117 +1,68 @@
-#include <iostream>       // std::cerr
-#include <stdexcept>      // std::out_of_range
-#include <vector>         // std::vector
-#include <cstdlib>				// rand function
+//c++ -o code code.cpp
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 /*
-	c++ -o code code.cpp -std=c++11
-	try/catch-исключения. Глава 5. Упражнение 11. Игра "Быки и коровы".
+	Глава 6. Упражнение 4. Использование конструкторов при инициализации
+	объектов класса.
 */
-const int v_size = 4;		//Размер вектора для отгадывания
 
-//загадывание значений вектора компьютером
-vector<int> make_vector()
-{
-	vector<int> v(v_size);
+class Name_value {
+public:
+	string name;
+	int score;
+	Name_value(string n) :name(n), score(0) {}
+	Name_value(int s) :name("test"), score(s) {}
+	Name_value(string n, int s) :name(n), score(s) {}
+};
 
-	for (int i = 0; i < v_size; i++)
-		v[i] = rand() % 10;
-	
-	return v;
-}
-
-//Ввод вектора пользователя для угадывания
-vector<int> input_vector()
-{
-	vector<int> p;
-	int element,i = 0;
-
-	while (i < v_size)
-	{
-		cin >> element;
-		if (element < 0 || 9 < element)
-			throw runtime_error("Значения чисел должны быть типа int \
-быть в пределах [0-9]!\n");
-		p.push_back(element);
-		i++;
-	}
-
-	return p;
-}
-
-int output_vector(vector<int> v)
-{
-	for (int i = 0; i < v.size(); i++)
-		cout << v[i] << " ";
-
-	cout << endl;
-}
-
-bool compare_vectors(vector<int> v, vector<int> p, int attempt)
-{//Ищем угадал ли пользователь число и его позицию
-	int bull = 0, cow = 0;
-	vector<int> v_search, p_search;
-	v_search = v;
-	p_search = p;
-	//сперва ищем быков, предварительно их помечая
-	for (int i = 0; i < v_size; i++)
-		if (p_search[i] == v_search[i])
-		{
-			bull++;
-			v_search[i]=-1; //убираем даный элемент из дальнейшей проверки
-			p_search[i]=-2; //убираем даный элемент из дальнейшей проверки
-		}
-	//потом коров
-	for (int i = 0; i < v_size; i++)
-		for (int j = 0; j < v_size; j++)
-			if (p_search[i] == v_search[j])
-			{
-			cow++;
-			v_search[j] = -1;
-			break;
-			}	
-
-	if (bull == 4)
-	{
-		cout << "Поздравляю Вы угадали c " << attempt << " попытки!\n";
-		return false;
-	}
-	else
-	{
-		cout << bull << " бык(-ов) и " << cow << " коровов(ы)\n";
-		return true;	//Сообщаяем, что нужно продолжить игру, т.к. не угадали всех быков
-	}
-}
-
-//--------------------------------------------------------------------------------
 int main () {
+	bool search;
+	int score;
 
-try {
-	vector<int> v;
-	vector<int> p;
-	bool next = true;
-	srand (time(NULL));
+	//the test initialization
+	Name_value t0("QWER");
+	Name_value t1(7);
 
-	v = make_vector();	//загадываем вектор
-	output_vector(v);
+//инициализация
+	vector<Name_value> v;						//don`t working
+	v.push_back(Name_value("Konstantin",7));
+	v.push_back(Name_value("Margarita",6));
+	v.push_back(Name_value("Irina",9));
+	v.push_back(Name_value("Sergey",3));
+	v.push_back(Name_value("Vadik"));		// Vadik,0
+	v.push_back(Name_value(9));					// test, 9
+	v.push_back(Name_value("Kos",5));
+	v.push_back(Name_value("Garry",3));
+	v.push_back(Name_value("KOnst)df2",5));
+	v.push_back(Name_value("Margo",2));
+	v.push_back(Name_value("Indiana",5));
+	v.push_back(Name_value("Djonse",3));
+	v.push_back(Name_value("Djonce",1));
+	v.push_back(Name_value("Bionce",4));
+	cout << v[v.size()-1].name << " " << v[v.size()-1].score << endl;	
 
-	// начинаем игру в угадывание
-	cout << "Введите вектор из " << v_size << " элементов для отгадывания:\n";
-	int attempt = 1;
-	while (next)
-	{
-		p = input_vector();
-		if ( next = compare_vectors(v,p,attempt) )
-			cout << "попробуйте ещё раз!\n";
-		attempt++;
-	}
-}
-catch (exception& e) {
-  cerr << "Ошибка-прерывание: " << e.what() << '\n'; 
-  return 1;
-}
+	//INPUT
+	cout << "Для пойска введите количество баллов и нажмите [ENTER]:\n";
+	while (cin >> score)
+		{
+		search = false;
+		for (int i = 0; i < v.size(); i++)
+			{
+			if (score == v[i].score)
+				{
+				cout << "(" << v[i].name << "," << v[i].score << ")\n";
+				search = true;
+				}
+			}
+		if (!search)  cout << "Score not found!\n";
+		}
+	//COMPUTING
+	
 
-return 0;
+	return 0;
 }
 
