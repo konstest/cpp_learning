d9ce787a4497a62a1f1681b5327ebfcce8611243
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-05-06 10:19:12 +0000

    Глава 8. Упражнение 6. Реализация аналога swap() для векторов типа string.

--- code.cpp
+++ code.cpp
@@ -6,16 +6,16 @@
 using namespace std;
 /*
 	c++ -o code code.cpp -std=c++11
-	Глава 8. Упражнение 5. Реализовать две функции меняющие последовательность
-	чисел на обратную в векторе.
+	Глава 8. Упражнение 6. Реализовать две функции меняющие последовательность
+	элементов вектора на обратную.
 */
 
 //------------------------------------------------------------------------------
 //Не изменяю первоначального вектора создаёт новый вектор собратной
 // последовательностью элементов
-vector<int> swap_x(vector<int> v)
+vector<string> swap_x(vector<string> v)
 {
-	vector<int> x;
+	vector<string> x;
 	for (int i=v.size(); i>0; i--)
 		x.push_back(v[i-1]);
 	return x;
@@ -23,14 +23,14 @@ vector<int> swap_x(vector<int> v)
 //------------------------------------------------------------------------------
 //Изменяет последовательность элементов на обратную в переданном ей векторе
 
-void swap_y(vector<int>& v)
+void swap_y(vector<string>& v)
 {
 	for (int i=0; i<v.size()/2; i++)
 		swap(v[i],v[v.size()-i-1]);
 }
 //------------------------------------------------------------------------------
 //Вывод вектора на экран
-void print(const string& comment, const vector<int>& v)
+void print(const string& comment, const vector<string>& v)
 {
 	cout << comment << endl;
 	for (int i=0; i< v.size(); i++)
@@ -41,14 +41,14 @@ void print(const string& comment, const vector<int>& v)
 //------------------------------------------------------------------------------
 int main ()
 try {
-	vector<int> f;
+	vector<string> f;
 	cout << "Вводите элементы вектора: ";
-	int elem;
+	string elem;
 	while (cin >> elem){
 		f.push_back(elem);
 	}
 	
-	vector<int> f2 = swap_x(f);
+	vector<string> f2 = swap_x(f);
 	print ("Вектор f после f2 = swap_x(f):",f);
 	swap_y(f);
 	print ("Вектор f после swap_y(f):",f);
