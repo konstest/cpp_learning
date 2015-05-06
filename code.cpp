87cc54510ecd87766a8d13c5be1e27fe19ae7407
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-05-06 13:18:23 +0000

    Глава 8. Упражнение 9. Написать функцию генерирующую псевдослучайные числа в диапазоне [a:b].

--- code.cpp
+++ code.cpp
@@ -3,26 +3,26 @@
 using namespace std;
 /*
 	c++ -o code code.cpp -std=c++11
-	Глава 8. Упражнение 8. Написать функцию генерирующую псевдослучайные числа
-	в диапазоне [0:MAXINT].
+	Глава 8. Упражнение 9. Написать функцию генерирующую псевдослучайные числа
+	в диапазоне [a:b].
 */
 
 //------------------------------------------------------------------------------
 
-unsigned int randint()
+unsigned int rand_in_range(unsigned int a, unsigned int b)
 {
-	return rand() % (unsigned int)0xffffffff;
+	return rand() % b + a;
 }
 
 //------------------------------------------------------------------------------
 int main ()
 try {
 	srand (time(NULL));
-	cout << "Введите количество чисел, для генерации: ";
-	int n=0;
-	cin >> n;
+	cout << "Введите количество чисел, для генерации и диапазон (n a b): ";
+	int n=0,a=0,b=0;
+	cin >> n >> a >> b;
 	for (int i=0; i<n; i++)
-		cout << i+1 << ": " << randint() << endl;
+		cout << i+1 << ": " << rand_in_range(a,b) << endl;
 //	cout << (unsigned int)0xffffffff << endl;
 	return 0;
 }
