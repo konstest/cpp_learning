82bc446645defe8dd572275c3e1838f65a1f5f32
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-06-03 12:34:40 +0000

    Глава 11. Упражнение 10. Напишите функцию split(s,w) возвращающую вектор подстрок аргумента , разделённых как пробелами так и символами из .

--- code.cpp
+++ code.cpp
@@ -1,6 +1,6 @@
 //
-//	Глава 11. Упражнение 9. Напишите функцию split(s) возвращающую вектор подстрок
-//	аргумента $s, разделённых пробелами.
+//	Глава 11. Упражнение 10. Напишите функцию split(s,w) возвращающую вектор подстрок
+//	аргумента $s, разделённых как пробелами так и символами из $w.
 //
 
 #include "code.h"
@@ -25,6 +25,16 @@ void print(const vector<string>& v)
 	for (int i=0; i<v.size(); i++)
 		cout << v[i] << endl;
 }
+
+//------------------------------------------------------------------------------
+
+void whitespace(string& s, const string spliters)
+{
+	for (int i=0; i<s.size(); i++)
+		for (int j=0; j<spliters.size(); j++)
+			if (s[i] == spliters[j]) s[i]=' ';
+}
+
 //------------------------------------------------------------------------------
 
 int main()
@@ -33,6 +43,7 @@ try
 	string str;
 	cout << "Input line:\n";
 	getline(cin,str);
+	whitespace(str,"!@#$%^&*()№?_+-:.,/{}[]`~");
 	print(split(str));
     return 0;
 }
