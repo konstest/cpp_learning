2650cdcafc0333968ed78076a5c779dcca325ca9
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-06-04 05:18:19 +0000

    Упражнение 13. Считаем количество символов каждой категории файла($имя) и записываем их в отдельный файл $имя.charstypes

--- code.cpp
+++ code.cpp
@@ -1,67 +1,86 @@
 //
-//	Глава 11. Упражнение 12. Изменение порядка слов в файле:
-//	What you name? -> name? you What
+//	Глава 11. Упражнение 13. Считаем количество символов каждой категории и
+//	записываем их в отдельный файл $имя.charstypes.
+//
 
 #include "code.h"
 
-using namespace std;
+//------------------------------------------------------------------------------
+struct charstypes {
+	charstypes(): whitespace(0), alpha_upper(0), alpha_lower(0), digit(0),
+	cntrl(0), other(0), chars_count(0) {}
+	int	whitespace;	//разделитель
+	int	alpha_upper;//буква в верхнем рекистре
+	int	alpha_lower;//буква в нижнем рекистре
+	int digit;		//цифра
+	int cntrl;		//Управляющий символ (ASCII 0..31 и 127)
+	int other;		//Просто какой то символ, не попавший в выше перечисл-е
+	int chars_count;//All chars count.
+};
 
 //------------------------------------------------------------------------------
 
 string read_file(const string& filename)
 {
-	ifstream ist(filename.c_str());
-	if (!ist) error("Don`t read this file: "+filename);
-	char ch;
 	string line;
+	ifstream ist(filename.c_str());
+	if (!ist) error("Don`t read this filename:",filename);
+	char ch;
 	while (ist.get(ch)) {
 		line += ch;
 		if (ist.eof()) break;
 	}
 	return line;
 }
+
 //------------------------------------------------------------------------------
 
-vector<string> split(const string& s)
+charstypes computing(const string& v)
 {
-	string word;
-	vector<string> v;
-	istringstream is(s);
-	while (is >> word) v.push_back(word);
-	return v;
+	int i;
+	charstypes chars;
+	chars.chars_count = v.size();
+	for (i=0; i<v.size(); i++) {
+		if (isspace(v[i])) chars.whitespace++;
+		if (isalpha(v[i])) {
+			if (isupper(v[i])) chars.alpha_upper++;
+			else chars.alpha_lower++;
+		}
+		if (isdigit(v[i])) chars.digit++;
+		if (iscntrl(v[i])) chars.cntrl++;
+		if (ispunct(v[i])) chars.other++;
+	}
+	chars.chars_count = i;
+	return chars;
 }
 
 //------------------------------------------------------------------------------
 
-void print(const vector<string>& v)
+void write_file(const string& filename, const charstypes& chars)
 {
-	for (int i=0; i<v.size(); i++)
-		cout << v[i] << " ";
-}
-
-void revers_write(const string& filename, const vector<string>& v)
-{
-	ofstream out(filename.c_str());
-	if (!out) error("Don`t read this file: "+filename);
-	for (int i=v.size()-1; i>=0; i--)
-		out << v[i] << " ";
+	ostringstream out;
+	out << "Файл "+filename+" содержит " << chars.chars_count << " сиволов, из которых:\n";
+	out << chars.whitespace << " - разделителей,\n";
+	out << chars.alpha_upper  << " - букв в верхнем регистре,\n";
+	out << chars.alpha_lower  << " - букв в нижнем регистре,\n";
+	out << chars.digit << " - цифр,\n";
+	out << chars.cntrl << " - управляющих символов (ASCII 0..31 и 127),\n";
+	out << chars.other << " - символов не попавший в выше перечисленные.\n";
+	cout << out.str();
+	ofstream f(filename+".chatstypes");
+	if (!f) error("Don`t read this file: "+filename+".chatstypes");
+	f << out.str();
 }
 //------------------------------------------------------------------------------
 
 int main()
-try
-{
+try {
+	cout << "Input filename:\n";
 	string filename;
-	cout << "Input filename: ";
 	cin >> filename;
-	string line = read_file(filename);
-	vector<string> v = split(line);
-	print(v);
-	cout << endl;
-	revers_write(filename,v);
-	line = read_file(filename);
-	v = split(line);
-	print(v);
+	string l = read_file(filename);
+	charstypes chars = computing(l);
+	write_file(filename,chars);
     return 0;
 }
 catch (...) {
