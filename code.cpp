f757993f6344d5854da702399e80e3a06bcb286d
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-07-23 11:48:57 +0000

    Глава 13. Упражнение 4. Определить функции n(), s(), e(), w(), center(), ne(), se() и т.д. Каждая из них должна получать аргумент типа Rectangle и возвращать объект типа Point. Иными словами функции вычисляющие координаты точек углов, центра и середины сторон прямоугольника.

--- code.cpp
+++ code.cpp
@@ -1,5 +1,7 @@
 //
-// Глава 13. Упражнение 3. Создать класс Arrow рисующий стрелки.
+// Глава 13. Упражнение 4. Определить функции n(), s(), e(), w(), center(), ne(), se() и т.д.
+// Каждая из них должна получать аргумент типа Rectangle и возвращать объект типа Point.
+// Иными словами функции вычисляющие координаты точек углов, центра и середины сторон прямоугольника.
 // c++ -o code code.cpp GUI/Simple_window.cpp GUI/Graph.cpp GUI/GUI.cpp GUI/Window.cpp -lfltk -lfltk_images -std=c++11
 
 #include "code.h"
@@ -7,86 +9,57 @@
 using namespace Graph_lib;
 
 //------------------------------------------------------------------------------
-
-void Arc::draw_lines() const
-{
-    if (color().visibility())
-        fl_arc(point(0).x,point(0).y,w+w,h+h,a_b,a_e);
+//North
+Point n(const Rectangle& ABCD) {
+	return Point(ABCD.point(0).x + ABCD.width()/2, ABCD.point(0).y);
 }
 
 //------------------------------------------------------------------------------
-
-Point Arc::center()	const
-{
-	return Point(point(0).x+w,point(0).y+h);
+//North-West
+Point nw(const Rectangle& ABCD) {
+	return ABCD.point(0);
 }
 
 //------------------------------------------------------------------------------
-// Rotation дуги
-void rotation (Arc& A)
-{
-	int b = A.angle_begin(), e = A.angle_end();
-	if (e<360) { b+=90; e+=90; } 
-	else { b=0; e=90; }
-	A.set_angle(b,e);
+//North-East
+Point ne(const Rectangle& ABCD) {
+	return Point(ABCD.point(0).x + ABCD.width(), ABCD.point(0).y);
 }
 
 //------------------------------------------------------------------------------
-
-void Box::draw_lines() const
-{
-    if (color().visibility()) {
-    	//draw arcs
-        fl_arc(point(0).x,point(0).y,a_w+a_w,a_h+a_h,90,180);
-        fl_arc(point(0).x+w-(a_w+a_w),point(0).y,a_w+a_w,a_h+a_h,0,90);
-        fl_arc(point(0).x,point(0).y+h-(a_h+a_h),a_w+a_w,a_h+a_h,180,270);
-        fl_arc(point(0).x+w-(a_w+a_w),point(0).y+h-(a_h+a_h),a_w+a_w,a_h+a_h,270,360);
-		//draw lines
-        fl_line(point(0).x,point(0).y+a_h,point(0).x,point(0).y+h-a_h);
-        fl_line(point(0).x+w,point(0).y+a_h,point(0).x+w,point(0).y+h-a_h);
-        fl_line(point(0).x+a_w,point(0).y,point(0).x+w-a_w,point(0).y);
-        fl_line(point(0).x+a_w,point(0).y+h,point(0).x+w-a_w,point(0).y+h);
-    }
+//East
+Point e(const Rectangle& ABCD) {
+	return Point(ABCD.point(0).x + ABCD.width(), ABCD.point(0).y + ABCD.height()/2);
 }
 
 //------------------------------------------------------------------------------
-
-Arrow::Arrow (Point p1, Point p2)
-: r1(r1), r2(r2)
-{
-	//Узнаём длину отрезка (нашей стрелки)
-	double width_arrow = sqrt( double(pow(p2.x-p1.x,2) + pow(p2.y-p1.y,2)) );
-	r1 = width_arrow/10;
-	r2 = r1/2;
-	add(p1);
-	add(p2);
+//South-East
+Point se(const Rectangle& ABCD) {
+	return Point(ABCD.point(0).x + ABCD.width(), ABCD.point(0).y + ABCD.height());
 }
 
 //------------------------------------------------------------------------------
+//South
+Point s(const Rectangle& ABCD) {
+	return Point(ABCD.point(0).x + ABCD.width()/2, ABCD.point(0).y + ABCD.height());
+}
 
-void Arrow::draw_lines()	const
-{
-	if (color().visibility()) {
-		//Узнаём длину отрезка (нашей стрелки)
-		double width_arrow = sqrt( double(pow(point(1).x-point(0).x,2) + pow(point(1).y-point(0).y,2)) );
-		//через отношение ищем координаты нашей второй окружности лежащей на
-		//расстоянии r1 от точки point(1), точки куда указывает стрелка
-		double lambda = r1 / width_arrow;
-		//Узнаём координаты второй окружности с радиусом r2
-		int xM = (point(1).x + lambda*point(0).x)/(1+lambda);
-		int yM = (point(1).y + lambda*point(0).y)/(1+lambda);
-		//Вычислим координаты точек линий стрелки (>) для этого возмём единичный 
-		//вектор от P1 до P2, повернём его на +90 градусов для P3 (-90 для P4) и умножим на r2.
-		int P3x = xM + (point(1).y - yM)*r2/r1;
-		int P3y = yM - (point(1).x - xM)*r2/r1;
-		int P4x = xM - (point(1).y - yM)*r2/r1;
-		int P4y = yM + (point(1).x - xM)*r2/r1;
-		//рисуем указатель (>)
-	    fl_line(point(1).x,point(1).y,P3x,P3y );
-	    fl_line(point(1).x,point(1).y,P4x,P4y );
-	}
-	//Дорисовываем саму (длинную) линию стрелки
-	Shape::draw_lines();
+//------------------------------------------------------------------------------
+//South-West
+Point sw(const Rectangle& ABCD) {
+	return Point(ABCD.point(0).x, ABCD.point(0).y + ABCD.height());
+}
+
+//------------------------------------------------------------------------------
+//West
+Point w(const Rectangle& ABCD) {
+	return Point(ABCD.point(0).x, ABCD.point(0).y + ABCD.height()/2);
+}
+
+//------------------------------------------------------------------------------
+//Center Rectangle
+Point center(const Rectangle& ABCD) {
+	return Point(ABCD.point(0).x + ABCD.width()/2, ABCD.point(0).y + ABCD.height()/2);
 }
 
 //------------------------------------------------------------------------------
@@ -94,19 +67,88 @@ void Arrow::draw_lines()	const
 int main ()
 try
 {
-    Simple_window win(Point(10,10),1600,1200,"Chapter 13. Exercise 3:");
+    Simple_window win(Point(10,10),1200,800,"Chapter 13. Exercise 3:");
 
-    int w=50, h=50, b=0, e=90;
+    Rectangle ABCD(Point(100,150),Point(400,300));
+    ABCD.set_style(Line_style(Line_style::solid,7));
+    win.attach(ABCD);
 
-    Arrow A(Point(400,700),Point(1000,150));
-    A.set_color(Color::yellow);
-    A.set_style(Line_style(Line_style::solid,9));
-    win.attach(A);
+	ostringstream oss;
+	Point P = nw(ABCD);
+	oss << "(" << P.x << "," << P.y << ")";
+	Text TXT(Point(P.x-35,P.y-5), oss.str());
+	TXT.set_font_size(15);
+	win.attach(TXT);
+	oss.str("");
 
-    Arrow B(Point(400,700),Point(1000,150));
-    B.set_color(Color::blue);
-    B.set_style(Line_style(Line_style::solid,2));
-    win.attach(B);
+	P = n(ABCD);
+	oss << "(" << P.x << "," << P.y << ")";
+	Text TXT1(Point(P.x-35,P.y-5), oss.str());
+	TXT1.set_font_size(15);
+	win.attach(TXT1);
+	oss.str("");
+
+	P = ne(ABCD);
+	oss << "(" << P.x << "," << P.y << ")";
+	Text TXT2(Point(P.x-35,P.y-5), oss.str());
+	TXT2.set_font_size(15);
+	win.attach(TXT2);
+	oss.str("");
+	
+	P = ne(ABCD);
+	oss << "(" << P.x << "," << P.y << ")";
+	Text TXT3(Point(P.x-35,P.y-5), oss.str());
+	TXT3.set_font_size(15);
+	win.attach(TXT3);
+	oss.str("");
+
+	P = se(ABCD);
+	oss << "(" << P.x << "," << P.y << ")";
+	Text TXT4(Point(P.x-35,P.y-5), oss.str());
+	TXT4.set_font_size(15);
+	win.attach(TXT4);
+	oss.str("");
+
+	P = s(ABCD);
+	oss << "(" << P.x << "," << P.y << ")";
+	Text TXT5(Point(P.x-35,P.y-5), oss.str());
+	TXT5.set_font_size(15);
+	win.attach(TXT5);
+	oss.str("");
+
+	P = sw(ABCD);
+	oss << "(" << P.x << "," << P.y << ")";
+	Text TXT6(Point(P.x-35,P.y-5), oss.str());
+	TXT6.set_font_size(15);
+	win.attach(TXT6);
+	oss.str("");
+
+	P = w(ABCD);
+	oss << "(" << P.x << "," << P.y << ")";
+	Text TXT7(Point(P.x-35,P.y-5), oss.str());
+	TXT7.set_font_size(15);
+	win.attach(TXT7);
+	oss.str("");
+
+	P = center(ABCD);
+	oss << "(" << P.x << "," << P.y << ")";
+	Text TXT8(Point(P.x-35,P.y-5), oss.str());
+	TXT8.set_font_size(15);
+	win.attach(TXT8);
+	oss.str("");
+
+	P = e(ABCD);
+	oss << "(" << P.x << "," << P.y << ")";
+	Text TXT9(Point(P.x-35,P.y-5), oss.str());
+	TXT9.set_font_size(15);
+	win.attach(TXT9);
+	oss.str("");
+
+    Rectangle abcd(Point(150,250),Point(500,600));
+    abcd.set_color(Color::blue);
+    abcd.set_style(Line_style(Line_style::solid,2));
+    win.attach(abcd);
+	win.attach(TXT);
 
     win.wait_for_button();    
 }
