c11bd69d039e3d01ff7f86f04f0c3a64082fc9dd
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-08-14 04:51:18 +0000

    Глава 14. Упражнение 2 - 3.

--- code.cpp
+++ code.cpp
@@ -1,139 +1,46 @@
 //
-// Глава 14. Упражнение 1. Определите два класса, Smiley и  Frowny, производные от класса Circle и рисующие два глаза и рот.
-// Затем создайте классы, производные от классов Smiley и Frowny, добавляющие к каждому из них свою шляпу.
+// Глава 14. Упражнение 2 - 3.
 // cleab1 && c++ -o code GUI/Simple_window.cpp GUI/Gb1aph.cpp GUI/GUI.cpp GUI/Window.cpp code.cpp -lfltk -lfltk_images -std=c++11 && ./code
 
 #include "code.h"
-/*
-	После 14-ой главы пришло понимание наследования и других возможностей ООП.
-	В связи с этим оглядываясь на предидущие упражнения я делаю вывод, что 
-	сейчас я бы их переделал, т.к. там часто используется дублирование кода и 
-	не используются уже реализованные возможности графического движка GUI.
-	Но переделывать я их не буду, т.к. наличие этих "не красиво", но решённых
-	упражнений отображает мой путь в осознании фундаментальных основ языка С++.
-*/
-//------------------------------------------------------------------------------
-
-void Hat_Smiley::draw_lines() const
-{
-    if (color().visibility()) {
-    	hat.draw_lines();
-		Smiley::draw_lines();
-	}
-}
-
-Hat_Smiley::Hat_Smiley(Point p, int r)
-:Smiley(p,r)
-{
-	hat.add(Point(center().x,center().y-1.3*radius()));
-	hat.add(Point(center().x+radius(),center().y-0.8*radius()));
-	hat.add(Point(center().x-radius(),center().y-0.8*radius()));
-}
-
-Hat_Frowny::Hat_Frowny(Point p, int r)
-:Frowny(p,r)
-{
-	hat.add(Point(center().x-0.6*radius(),center().y-1.2*radius()));
-	hat.add(Point(center().x+0.6*radius(),center().y-1.2*radius()));
-	hat.add(Point(center().x+radius(),center().y-0.6*radius()));
-	hat.add(Point(center().x-radius(),center().y-0.6*radius()));
-}
-
-void Hat_Frowny::draw_lines() const
-{
-    if (color().visibility()) {
-    	hat.draw_lines();
-		Frowny::draw_lines();
-	}
-}
-
-//Конструктор Smiley
-Smiley::Smiley(Point p, int r)
-:Circle(p,r),
-l_eye(Point(center().x-radius()/3-radius()/7,center().y-radius()/3),"x"),
-r_eye(Point(center().x+radius()/3,center().y-radius()/3),"__"),
-smile(Point(center().x,center().y-radius()/3),radius()/1.5,radius(),220,320)
-{
-	l_eye.set_font_size(radius()/5);
-	r_eye.set_font_size(radius()/5);
-}
-
-void Smiley::draw_lines() const
-{
-    if (color().visibility()) {
-    	l_eye.draw_lines();
-    	r_eye.draw_lines();
-    	smile.draw_lines();
-		Circle::draw_lines();
-    }
-}
-
-//Конструктор Frowny
-Frowny::Frowny(Point p, int r)
-:Circle(p,r),
-l_eye(Point(center().x-radius()/3-radius()/7,center().y-radius()/3),"/"),
-r_eye(Point(center().x+radius()/3,center().y-radius()/3),"\\"),
-smile(Point(center().x,center().y+radius()),radius()/1.5,radius(),40,140)
-{
-	l_eye.set_font_size(radius()/5);
-	r_eye.set_font_size(radius()/5);
-}
-
-void Frowny::draw_lines() const
-{
-    if (color().visibility()) {
-    	l_eye.draw_lines();
-    	r_eye.draw_lines();
-    	smile.draw_lines();
-		Circle::draw_lines();
-    }
-}
-
 
 //------------------------------------------------------------------------------
-// Добавил класс дугу из главы 13 упражнение 1 (SVN version - 82)
-void Arc::draw_lines() const
-{
-    if (color().visibility())
-        fl_arc(point(0).x,point(0).y,w+w,h+h,a_b,a_e);
-}
+class Abstract {
+public:
+	virtual void inc(int ii) =0;
+	void val(int i) { v = i; }
+	int i() { return v; }
+private:
+	int v;
+};
 
-
-Point Arc::center()	const
-{
-	return Point(point(0).x+w,point(0).y+h);
-}
-
-void rotation (Arc& A)
-{
-	int b = A.angle_begin(), e = A.angle_end();
-	if (e<360) {
-		b += 90;
-		e += 90;
-	}
-	else {
-		b = 0;
-		e = 90;
-	}
-	A.set_angle(b,e);
-}
+class subclass : public Abstract {
+public:
+	void inc(int ii) { val(i() + ii); }
+};
 //------------------------------------------------------------------------------
 
 int main ()
 try
 {
+/*
+Exercise 14.2:
 	Simple_window win(Point(100,100),1600,1400,"Chapter 14.");
-	Hat_Smiley S(Point(300,300),200);
-	S.set_style(Line_style(Line_style::solid,6));
-	S.set_color(Color::red);
-	win.attach(S);
-
-	Hat_Frowny F(Point(800,500),200);
-	F.set_style(Line_style(Line_style::solid,5));
-	F.set_color(Color::blue);
-	win.attach(F);
+	Circle S(Point(300,300),200);
+	Circle F(Point(800,500),200);
+	//Error!
+//	F = S;	//code.cpp:26:6: error: use of deleted function ‘Graph_lib::Circle& Graph_lib::Circle::operator=(const Graph_lib::Circle&)’
 
 	win.wait_for_button();       // Display!
+*/
+//Exercise 14.3:
+
+	subclass i;
+	for (i.val(0); i.i() < 7; i.inc(1))
+		cout << i.i() << endl;
+	
+	//Error!
+	Abstract j;	//code.cpp:44:11: error: cannot declare variable ‘j’ to be of abstract type ‘Abstract’
 }
 catch(exception& e) {
     return 1;
--- code.h
+++ code.h
@@ -9,68 +9,6 @@
 #include <cmath>
 
 //------------------------------------------------------------------------------
-//Класс рисующий дугу
-struct Arc : Shape {
-	//p - центрб, w,h - дистанции от центра по ширине и по высоте, a - угол
-	Arc (Point p, int w, int h, int a_b, int a_e)
-        : w(w), h(h), a_b(a_b), a_e(a_e)
-    { 
-        add(Point(p.x-w,p.y-h));
-    }
-	
-	void draw_lines()	const;
-	
-	void set_major(int ww) { w = ww; }
-	void set_minor(int hh) { h = hh; }
-	void set_angle(int aa_b, int aa_e) { a_b = aa_b; a_e = aa_e; }
-	
-	int angle_begin()	const { return a_b; }
-	int angle_end()		const { return a_e; }
-	
-	Point	center()	const;
 
-private:
-	int w;	//ширина
-	int h;	//высота
-	int a_b;	//начальный угол
-	int a_e;	//конечный угол
-};
-
-//Рисует счастливое лицо
-class Smiley : public Circle {
-public:
-	Smiley(Point p, int r);		//конструктор
-	void draw_lines() const;
-	Text l_eye;		// метка левого глаза
-	Text r_eye;		// правого
-private:
-    Arc smile;		// улыбка в виде дуги
-};
-
-//Рисует счастливое лицо в шапке-китайке :)
-struct Hat_Smiley : Smiley {
-	Hat_Smiley(Point p, int r);				//конструктор
-	void draw_lines() const;
-private:
-	Polygon	hat;
-};
-
-//Рисует грустное лицо
-struct Frowny : Circle {
-	Frowny(Point p, int r);		//конструктор
-	void draw_lines() const;
-	Text l_eye;		// метка левого глаза
-	Text r_eye;		// правого
-private:
-    Arc smile;		// рот в виде дуги
-};
-
-//Рисует грустное лицо с тазом на голове :)
-struct Hat_Frowny : Frowny {
-	Hat_Frowny(Point p, int r);	//конструктор
-	void draw_lines() const;
-private:
-	Polygon	hat;
-};
 //------------------------------------------------------------------------------
 
