970fe87e1816868769c4bfe155147fcc26b7c3e1
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-09-14 15:37:38 +0000

    Глава 14. Упражнение 9. Определить класс Group служащий контейнером объектов класса Shape с удобными операциями над членами класса Group. Подсказака вектор: Vector_ref. Используя класс Group, определите класс, рисующий шахматную доску по которой шашки могут перемещаться под управлением программы.

--- code.cpp
+++ code.cpp
@@ -1,91 +1,120 @@
 //
-// Глава 14. Упражнение 8. Определить класс Octagon, реализующий правильный восьмиугольник. Напишите
-//тестовую программу, выполняющую все его функции-члены (определённые вами или унаследованные от класса Shape).
-//
+// Глава 14. Упражнение 9. Определить класс Group служащий контейнером объектов класса Shape с удобными операциями
+//над членами класса Group. Подсказака вектор: Vector_ref. Используя класс Group, определите класс, рисующий шахматную доску
+//по которой шашки могут перемещаться под управлением программы.
 // clear && c++ -o code GUI/Simple_window.cpp GUI/Graph.cpp GUI/GUI.cpp GUI/Window.cpp code.cpp -lfltk -lfltk_images -std=c++11 && ./code
 
 #include "code.h"
 
 static const double PI = 3.14159265359;
 
+const Color numbers_color(Color::red);	//Цвет боковых индексов шахматной доски
+const Color black_checkers_color(Color::black);	//Цвет чёрных шашек
+const Color white_checkers_color(Color::white);	//Цвет белых шашек
+const Color white_cells_color(Color::white);	//Цвет белых ячеек доски
+const Color black_cells_color(Color::blue);		//Цвет чёрных ячеек доски
+
 //------------------------------------------------------------------------------
 
-unsigned int randint(int arg)
+//	Инициализация ячейки фона
+void Checkers::draw_cell(int i, int j, Color C1, Color C2)
 {
-	return rand() % arg;
-}
-
-int rounding(double val)
-{
-	return ( (val - (int)val) >= 0.5 ) ? val+1 : val;
-}
-
-
-Point rotation(const Point& P, int angle, int r)
-{
-	double x = P.x - r*cos(angle*(PI/180));
-	double y = P.y - r*sin(angle*(PI/180));
-	return Point(rounding(x),rounding(y));
-}
-
-Octagon::Octagon(const Point& P, int rr ): r(rr)
-{
-	add(P);
-}
-
-void Octagon::draw_lines() const
-{
-	Point P1, P2;
-	vector<Point> tochki;
-    if (color().visibility()) {
-		for (int i=0; i<8; i++) {
-			P1 = rotation(point(0),i*45,r);
-			P2 = rotation(point(0),(i+1)*45,r);
-			tochki.push_back(P1);
-			tochki.push_back(P2);
-			fl_line(P1.x,P1.y,P2.x,P2.y);
-		}
+	board_items.push_back(new Rectangle(Point(i*cell_size,j*cell_size),cell_size,cell_size));
+	int last_added = board_items.size() - 1;
+	if (j%2 == 0) {
+		board_items[last_added].set_color(C2);
+		board_items[last_added].set_fill_color(C2);
 	}
-    if (fill_color().visibility()) {
-        fl_color(fill_color().as_int());
-        fl_begin_complex_polygon();
-        for(int i=0; i<tochki.size(); ++i){
-            fl_vertex(tochki[i].x, tochki[i].y);
-        }
-        fl_end_complex_polygon();
-        fl_color(color().as_int());    // reset color
-    }
+	else {
+		board_items[last_added].set_color(C1);
+		board_items[last_added].set_fill_color(C1);
+	}
 }
 
+void Checkers::draw_text(int i, int j, Color C, string S)
+{
+	numbers.push_back(new Text(Point(i*cell_size + cell_size/3,j*cell_size + cell_size/2), S));
+	numbers[numbers.size()-1].set_color(C);
+	numbers[numbers.size()-1].set_font_size(40);
+}
+
+int Checkers::draw_checker(int i, int j, Color C)
+{
+	board_items.push_back(new Circle(Point(i,j),cell_size/2 - cell_size/3));
+	int index = board_items.size()-1;
+	board_items[index].set_color(C);
+	board_items[index].set_style(Line_style(Line_style::solid,cell_size/2));
+	return index;
+}
+
+Checkers::Checkers()
+{
+	//инициализируем доску, её фон и т.д.
+	ostringstream oss;
+	for (int i=0; i<cell_count; i++) {
+		oss << i;
+		draw_text(cell_count,i,numbers_color,oss.str());
+		draw_text(i,cell_count,numbers_color,oss.str());
+		if (i%2==0)
+			for (int j=0; j<cell_count; j++)
+				draw_cell(i,j,black_cells_color,white_cells_color);
+		else
+			for (int j=0; j<cell_count; j++)
+				draw_cell(i,j,white_cells_color,black_cells_color);
+		oss.str("");
+	}
+
+	//инициализируем шашки
+	for (int i=1; i<cell_count; i+=2) {
+		int index;
+	//размещаяем белые шашки:
+		white_checkers.push_back(draw_checker(i*cell_size + cell_size/2,cell_size/2,Color::white));
+		white_checkers.push_back(draw_checker((i-1)*cell_size + cell_size/2,cell_size + cell_size/2,Color::white));
+		white_checkers.push_back(draw_checker(i*cell_size + cell_size/2,cell_size*2 + cell_size/2,Color::white));
+	//чёрные шашки:
+		black_checkers.push_back(draw_checker((i-1)*cell_size + cell_size/2,cell_count*cell_size-cell_size/2,Color::black));
+		black_checkers.push_back(draw_checker((i-1)*cell_size + cell_size/2,cell_count*cell_size - 2*cell_size - cell_size/2,Color::black));
+		black_checkers.push_back(draw_checker(i*cell_size + cell_size/2,cell_count*cell_size - cell_size - cell_size/2,Color::black));
+	}
+}
+
+void Checkers::draw_lines()	const
+{
+	for (int i=0; i<board_items.size(); i++)
+		board_items[i].draw();
+		
+	for (int i=0; i<numbers.size(); i++)
+		numbers[i].draw();
+}
+
+void Checkers::move(bool white, int index, int to_x, int to_y)
+{
+	if (white) {
+		Point P = board_items[white_checkers[index]].point(0);
+		board_items[white_checkers[index]].move(to_x*cell_size-(P.x-cell_size/2)-15,to_y*cell_size-(P.y-cell_size/2)-15);
+	}
+	else {
+		Point P = board_items[black_checkers[index]].point(0);
+		board_items[black_checkers[index]].move(to_x*cell_size-(P.x-cell_size/2)-15,to_y*cell_size-(P.y-cell_size/2)-15);
+	}
+}
 //------------------------------------------------------------------------------
 
 int main ()
 try
 {
-	Simple_window win(Point(10,10),1800,1000,"Chapter 14.");
+	Simple_window win(Point(10,10),900,900,"Chapter 14. Cheese 8x8.");
 
-	srand (time(NULL));
+	Checkers shashki;
+	win.attach(shashki);
+	
+	win.wait_for_button();
 
-	Octagon Oct(Point(300,300),100);
-	ostringstream oss;
-	Point P = Oct.center();
-	oss << P.x << "," << P.y;
-	Text lable(Point(P.x-35,P.y),oss.str());
-	lable.set_font_size(20);
-	win.attach(Oct);
-	win.attach(lable);
+	bool black = false, white = true;
+	shashki.move(white,2,2,3);
 	win.wait_for_button();
-	lable.set_label("");
-	Oct.set_center(Point(600,400));
-	win.wait_for_button();
-	Oct.set_radius(175);
-	win.wait_for_button();
-	Oct.set_color(Color(Color::blue));
-	win.wait_for_button();
-	Oct.set_style(Line_style(Line_style::dot,7));
-	win.wait_for_button();
-	Oct.set_fill_color(Color(Color::red));
-	//the end...
+	shashki.move(black,4,1,4);
+
 	win.wait_for_button();
 }
 catch(exception& e) {
--- code.h
+++ code.h
@@ -9,17 +9,36 @@
 #include <cmath>
 
 //------------------------------------------------------------------------------
+static const int cell_size = 100;
+static const int cell_count = 8;
 
-class Octagon : public Shape {
+class Group {
+    vector<Shape*> v;
 public:
-	Octagon(const Point& P, int rr );
-    void draw_lines() const;
-    int radius() const { return r; }
-    Point center() { return point(0); }
-    void set_center(const Point& P) { set_point(0,P); }
-    void set_radius(int rr) { r = rr; };
+	Group() {}
+
+    void push_back(Shape& s) { v.push_back(&s); }
+    void push_back(Shape* p) { v.push_back(p); }
+
+    Shape& operator[](int i) { return *v[i]; }
+    const Shape& operator[](int i) const { return *v[i]; }
+
+    int size() const { return v.size(); }
+};
+
+class Checkers : public Shape {
+public:
+	void draw_lines()	const;
+	Checkers();		//конструктор
+	void move(bool white_black, int index, int to_x, int to_y);//перемещение шашек
 private:
-    int r;
+	void draw_cell(int i, int j, Color C1, Color C2);	//инициализация ячейки фона
+	void draw_text(int i, int j, Color C1, string S);	//инициализация текста
+	int draw_checker(int i, int j, Color C);			//создаёт шашки
+	Group board_items;
+	vector<int> white_checkers;				// индексы белых шашек из board_items
+	vector<int> black_checkers;				// -|-
+	Vector_ref<Text> numbers;
 };
 
 //------------------------------------------------------------------------------
