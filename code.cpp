71ca1d8ad99fcbd5da25a63ac36538770bad7ae4
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-09-25 06:09:28 +0000

    Модифицируйте класс Binary_tree так, чтобы он мог рисовать разной формы узлы (например в виде треугольников).

--- code.cpp
+++ code.cpp
@@ -1,17 +1,109 @@
 /*
- Глава 14. Упражнение 11.
-		Определите класс Binary_tree, производный от класса Shape. 
- Введите параметр, задающий колличство уровней (levels==0 означает, что в дереве
- нет ни одного узла, levels==1 означает, что в дереве есть один узел, levels==2
- означает, что дерево состоит из вершины и двух узлов, levels==3 означает, что
- дерево состоит из вершины и двух дочерних узлов, которые в свою очередь имеют
- по два дочерних узла, и т.д.). Пусть узел изображается маленьким кружочком.
- Соедините узлы линиями (как это принято).
+ Глава 14. Упражнение 12.
+		Модифицируйте класс Binary_tree так, чтобы он рисовал свои узлы с помощью
+ виртуальной функции. Затем выведите из класса Binary_tree новый класс, в котором
+ эта виртуальная функция замещается так, что узлы изображаются иначе (например,
+ в виде треугольников).
  clear && c++ -o code GUI/Simple_window.cpp GUI/Graph.cpp GUI/GUI.cpp GUI/Window.cpp code.cpp -lfltk -lfltk_images -std=c++11 && ./code
 */
 
 #include "code.h"
 
+const double PI = 3.141592653589;
+
+//------------------------------------------------------------------------------
+//	Округление числа с плавающей точкой
+unsigned int rounding(double k)
+{
+	return ( (k-(int)k) < 0.5 ) ? k : k+1;
+}
+
+// Поворачиваем точку P на угол angle с радиусом R
+Point rotation(Point P, double angle, int R)
+{
+	double x = P.x - (double)R * cos((angle*PI)/180);
+	double y = P.y - (double)R * sin((angle*PI)/180);
+	return Point(rounding(x),rounding(y));
+}
+
+//конструктор создания правильной n- угольной звезды
+Star::Star(Point cc, int nn, int pp, int RR, int AA)
+:center_xy(cc), n(nn), p(pp), R(RR), r(0), A(AA)
+{ }
+
+//Конструктор создания звезды с ручной установкой радиуса внутренней окружности 
+Star::Star(Point cc, int nn, int pp, int RR, int rr, int AA)
+:center_xy(cc), n(nn), p(pp), R(RR), r(rr), A(AA)
+{ }
+
+// Отрисовка звезды
+void Star::draw_lines() const
+{
+	double angle = 360/n;	//определяем угол вершин
+	double a = 0 + A;
+	vector<Point> P;
+	if (r == 0) {
+		for (int i = 0; i < n; i++) {						//
+			P.push_back( rotation(center_xy, a, R) );			//ищем координаты
+			a = a + angle;									//вершин правильной
+		}													//звезды
+	}
+	else {
+		for (int i = 0; i < n; i++) {						//ищем координаты вершин
+			P.push_back( rotation(center_xy, a, R) );			//на внешней и
+			P.push_back( rotation(center_xy, a+angle/2, r) );	//на внутренней окружностях
+			a = a + angle;									//
+		}													//
+	}
+
+	int np = P.size();
+	if (r == 0) {//Рисование правильной звезды, не заполненной
+		if (color().visibility())
+			for (int i = 0; i < np; i++)
+				if (i+p < np - 1)//рисуется линия от точки минуя p кол-во вершин
+					fl_line(P[i].x,P[i].y,P[i+p+1].x,P[i+p+1].y);
+				else
+					fl_line(P[i].x,P[i].y,P[i+p-(np-1)].x,P[i+p-(np-1)].y);
+	}
+	else {//рисуем звезду с установленным внутренним радиусом
+		if (fill_color().visibility()) {				//
+		    fl_color(fill_color().as_int());			//
+		    fl_begin_complex_polygon();					//заполняем цветом
+		    for(int i=0; i<np; ++i)						//внутреннюю часть
+		        fl_vertex(P[i].x, P[i].y);				//звезды
+		    fl_end_complex_polygon();					//
+		    fl_color(color().as_int());		// reset color	//
+		}
+
+		if (color().visibility()) {
+		    for (unsigned int i=1; i<np; ++i)
+		        fl_line(P[i-1].x,P[i-1].y,P[i].x,P[i].y);
+		    fl_line(P[np-1].x,P[np-1].y,P[0].x,P[0].y);
+		}
+	}
+}
+
+// Установка радиуса внутренней окружности
+void Star::set_intradius(int rr)
+{
+	if (rr < R-10) r = rr;
+	else error ("Слишком большой радиус внтуренний окружности");
+}
+
+// Установка числа вершин пропуская которые соединяются рёбрами вершины, т.е.
+// в 5- конечной звезде через 1, в 7 уже можно и через 2, в 9 - 3, и.т.д.
+void Star::set_num_across_vert(int pp)
+{//Если рисуется не правильная звезда, а с вручную установленным радиусом, то
+// изменение количества промежуточных вершин нам ничего даст, т.к. там прорисовка
+// выполняется через дополнительный поворот координат лежащих на внутренней 
+// окружности ещё на половину угла angle/2, т.е. по середине между вершинами
+	if (r == 0) {
+		if ( (3 + pp*2) <= n)	//проверяем корректность установки 
+			p = pp;
+		else
+			error("Не правильное установка количества промежуточных вершин");
+	}
+}
 
 //------------------------------------------------------------------------------
 //Считает количество узлов на указанном уровне
@@ -29,7 +121,7 @@ int Binary_tree::nodes_count(int lvl)
 
 //------------------------------------------------------------------------------
 //Конструктор в котором просчитывается 
-Binary_tree::Binary_tree(Point xy, int l )
+Binary_tree::Binary_tree(Point xy, int l, Nodes_type t )
 : level(l)
 {
 	int y=0, x=0;
@@ -38,11 +130,23 @@ Binary_tree::Binary_tree(Point xy, int l )
 		x = width*(nodes_count(level)-1);
 		for (int i=level; i > 1; i--) {
 			y = (i - 1)*height;
-			for (int j = xy.x-x/2, k = 0; k < nodes_count(i); k++, j=j+x/(nodes_count(i)-1))
-				nodes.push_back(new Circle(Point(j,xy.y+y),links_radius));
+			for (int j = xy.x-x/2, k = 0; k < nodes_count(i); k++, j=j+x/(nodes_count(i)-1)){
+				if (t == Nodes_type::circle)
+					nodes.push_back(new Star(Point(j,xy.y+y), 14, 0, links_radius, 90));;
+				if (t == Nodes_type::star)
+					nodes.push_back(new Star(Point(j,xy.y+y), 5, 1, links_radius, 90));
+				if (t == Nodes_type::triangle)
+					nodes.push_back(new Star(Point(j,xy.y+y), 3, 0, links_radius, 90));
+			}
 			x = x - (x/(nodes_count(i)-1));
 		}
-		nodes.push_back(new Circle(xy,links_radius));	//root tree
+		//root tree
+		if (t == Nodes_type::circle)
+			nodes.push_back(new Star(xy, 13, 0, links_radius, 90));;
+		if (t == Nodes_type::star)
+			nodes.push_back(new Star(xy, 5, 1, links_radius, 90));
+		if (t == Nodes_type::triangle)
+			nodes.push_back(new Star(xy, 3, 0, links_radius, 90));
 	//Draw links:
 		int count = nodes_count(level),d = 0,i = 0,c = 0;
 		while ( count > 1 ) {
@@ -78,11 +182,11 @@ try
 {
 	int w = 1920;
 	int h = 1000;
-	Simple_window win(Point((x_max() - w)/2,(y_max() - h)/2),w,h,"Chapter 14. Exam 11.");
+	Simple_window win(Point((x_max() - w)/2,(y_max() - h)/2),w,h,"Chapter 14. Exam 12. Starship Troopers");
 
-	Binary_tree	tree(Point(win.x_max()/2,50),6);
+	Binary_tree	tree(Point(win.x_max()/2,50),6, Binary_tree::star);
 	tree.set_color(Color::blue);
-	tree.set_style(Line_style(Line_style::solid,4));
+	tree.set_style(Line_style(Line_style::solid,3));
 	win.attach(tree);
 	win.wait_for_button();
 }
--- code.h
+++ code.h
@@ -9,16 +9,43 @@
 
 using namespace Graph_lib;
 
+struct Star : Shape {
+	Star(Point c, int nn, int pp, int RR, int AA);//рисует "правильную" звезду
+	Star(Point c, int nn, int pp, int RR, int rr, int AA);//рисует практически какую хошь звезду :)
+	void set_angle(int AA) { A = AA; }			//меняет угол поворота
+	void set_intradius(int rr);					//меняет внутренний радиус
+	void set_radius(int RR) { R = RR; }			//меняет внешний радиус
+	void set_num_vert(int nn) { n = nn; }		//установить количество вершин
+	void set_num_across_vert(int pp);			//уст. кол. вершин через которое
+	void draw_lines()	const;
+	Point center() 	{ return center_xy; }
+	int radius() 	{ return R; }
+private:
+	Point center_xy;	//Координаты центра звезды 
+	int n;	//Количество вершин
+	int p;	//количество вершин через которое происходит соединение вершин
+			//т.е. 1 начиная с 5 конечных звёзд, 2 - 7-и, 3 - 9, и т.д.
+	int R;	//радиус внешней окружности
+	int r;	//радиус внутренней окружности
+	int A;	//угол поворота звезды
+};
+
 struct Binary_tree : Shape {
-    Binary_tree(Point xy, int level );
+	enum Nodes_type {
+		triangle,
+		circle,
+		star
+	};
+    Binary_tree(Point xy, int level, Nodes_type t);
     void draw_lines()	const;
 private:
 	int level;
 	int height = 75;			//растягивание по оси ординат между уровнями
 	int width = 50;				//по оси абцисс
 	int links_radius = 20;		//радиус узла
-	Vector_ref<Circle> nodes;	//хранение узлов
+	Vector_ref<Star> nodes;		//хранение узлов
 	Vector_ref<Lines> links;	//хранение связей
 	int nodes_count(int lvl);	//определениеколичества узолов на данном уровне
 };
+
 
