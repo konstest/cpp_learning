d7b93349a95839fe270b8fe4a0240e3afeef31e1
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-11-12 06:34:33 +0000

    Глава 15. Задание для повторение в конце главы. Class definition drill:

--- code.cpp
+++ code.cpp
@@ -1,84 +1,48 @@
 /*
  Глава 15.
-	Function graphing drill: 1-5.
-	Separate Shape for each function to be graphed: 1-9.
+	Class definition drill:: 1-9.
  clear && c++ -o code GUI/Simple_window.cpp GUI/Graph.cpp GUI/GUI.cpp GUI/Window.cpp code.cpp -lfltk -lfltk_images -std=c++11 && ./code
 */
 
 #include "code.h"
 #include "cmath"
 
+bool if_alnum(char ch)
+{
+	string str = ";:\"'[]*&^%$#@!";
+	for (int i=0; i<str.size(); i++)
+		if (ch == str[i])	return true;
+	return false;
+}
+
 //------------------------------------------------------------------------------
-double one(double x) { return 1; }
-double slope(double x) { return x/2; }
-double square(double x) { return x*x; }
-double sloping_cos(double x) { return cos(x)+slope(x); }
+Person::Person(string n, int a) {
+	if (a >= 0 && a <= 150)
+		second_name = a;
+	else
+		error("ERROR! Age should be within [0:150]");
+
+	for (int i=0; i<n.size(); i++)
+		if ( if_alnum(n[i]) ) error("The name must not contain: ;:\"'[]*&^%$#@!");
+	first_name = n;
+}
 
 //------------------------------------------------------------------------------
 int main ()
 try
-{		//x_win = x_max()*0.7
-    const int xmax = 600;    	// window size
-    const int ymax = 600;
-    const Point orig(xmax/2,ymax/2);
-
-    const int xlength = 400;    // length of axes
-    const int ylength = 400;
-
-    const int xoffset = (xmax-xlength)/2; // distance from left hand side of window to y axis
-    const int yoffset = (ymax-ylength)/2;  // distance from bottom of window to x axis
-
-    const double xscale = 20;					// scale of x values
-    const double yscale = 20;					// scale of y values 
-
-	Simple_window win(Point((1920-xmax)/2,(1080-ymax)/2),xmax,ymax,"Chapter 15. Exercises.");
-
-    Axis x(Axis::x, Point(xoffset,ymax/2), xlength, xlength/xscale,"1 == 20 pixels");
-    x.label.move(-100,0);
-    x.label.set_font_size(15);
-    x.set_color(Color::red);
-	win.attach(x);
-
-    Axis y(Axis::y, Point(xmax/2,ymax-yoffset), ylength, ylength/xscale,"1 == 20 pixels");
-    y.label.move(20,0);
-    y.label.set_font_size(15);
-    y.set_color(Color::red);
-	win.attach(y);
-	
-	const int x_range = -10;
-	const int y_range = 11;
-	const int points_count = 400;
-
-    Function s1(one, x_range, y_range, orig, points_count, xscale, yscale);
-    Text ts(Point(100,ymax/2-40),"one");
-    ts.set_font_size(15);
-	win.attach(s1);
-	win.attach(ts);
-
-    Function s2(slope, x_range, y_range, orig, points_count, xscale, yscale);
-    Text ts2(Point(100,ymax/2+ymax/4-20),"x/2");
-    ts2.set_font_size(15);
-	win.attach(s2);
-	win.attach(ts2);
-
-    Function s3(square, x_range, y_range, orig, points_count, xscale, yscale);
-    Text ts3(Point(xmax/2-100,20),"x*x");
-	ts3.set_font_size(15);
-	win.attach(s3);
-	win.attach(ts3);
-
-    Function s4(cos, x_range, y_range, orig, points_count, xscale, yscale);
-    s4.set_color(Color::blue);
-	win.attach(s4);
-
-    Function s5(sloping_cos, x_range, y_range, orig, points_count, xscale, yscale);
-    x.label.move(0,10);
-    x.notches.set_color(Color::dark_red);
-    win.attach(s5);
-
-	win.wait_for_button();
+{
+	Person P;
+	vector<Person> N;
+	for (int i=0; i<3; i++) {
+		cin >> P;
+		N.push_back(P);
+	}
+	cout << endl;
+	for (int i=0; i<3; i++)
+		cout << N[i];
 }
 catch(exception& e) {
+	cout << e.what() << endl;
     return 1;
 }
 catch(...) {
--- code.h
+++ code.h
@@ -10,5 +10,32 @@
 
 //------------------------------------------------------------------------------
 
+class Person {
+private:
+	string first_name;
+	int second_name;
+public:
+	Person() {}
+	Person(string n, int a);
+	int		age() 	const { return second_name; }
+	string	name()	const { return first_name; }
+};
+
+ostream& operator<<(ostream& os, Person& pe) {
+	return os << pe.name() << " " << pe.age() << endl;
+}
+
+istream& operator>>(istream& is, Person& pe) {
+	int age;
+	string name;
+	is >> name >> age;
+	if (!is) {
+		cout << "Error input format!\n";
+		return is;
+	}
+	pe = Person(name,age);
+	return is;
+}
+
 //------------------------------------------------------------------------------
 
