b6554ffa16136a5513fb183fec5f419912c3617d
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2015-12-06 16:58:58 +0000

    Chapter 16. Exercises 1-2.

--- code.cpp
+++ code.cpp
@@ -1,9 +1,9 @@
 /*
- Chapter 16. Drill.
-1. Make a completely new project with linker settings for FLTK (as described in Appendix D).
-2. Using the facilities of Graph_lib, type in the line-drawing program from §16.5 and get it to run.
-3. Modify the program to use a pop-up menu as described in §16.7 and get it to run.
-4. Modify the program to have a second menu for choosing line styles and get it to run.
+ Chapter 16. Exercises 1-2.
+	1. Make a My_window that’s a bit like Simple_window except that it has two buttons, next and quit.
+	2. Make a window (based on My_window) with a 4-by-4 checkerboard of square buttons. When pressed, a button
+performs a simple action, such as printing its coordinates in an output box, or turns a slightly different color (until another
+button is pressed).
 
  clear && c++ -o code GUI/Simple_window.cpp GUI/Graph.cpp GUI/GUI.cpp GUI/Window.cpp code.cpp -lfltk -lfltk_images -std=c++11 && ./code
 */
@@ -16,138 +16,68 @@ using namespace std;
 
 //------------------------------------------------------------------------------
 
-Lines_window::Lines_window(Point xy, int w, int h, const string& title)
+My_window::My_window(Point xy, int w, int h, const string& title)
     :Window(xy,w,h,title),
-    color_menu(Point(x_max()-70,30),70,20,Menu::vertical,"color"),
-    style_menu(Point(0,30),70,20,Menu::vertical,"style"),
-    menu_button(Point(x_max()-80,30), 80, 20, "color menu", cb_menu),
-    style_menu_button(Point(0,30), 80, 20, "style menu", cb_style_menu),
-    next_button(Point(x_max()-150,0), 70, 20, "Next point", cb_next),
     quit_button(Point(x_max()-70,0), 70, 20, "Quit", cb_quit),
-    next_x(Point(x_max()-310,0), 50, 20, "next x:"),
-    next_y(Point(x_max()-210,0), 50, 20, "next y:"),
-    xy_out(Point(100,0), 100, 20, "current (x,y):")
+    out(Point(70,0), 70, 20, "output")
 {
-    attach(next_button);
+	attach(out);
     attach(quit_button);
-    attach(next_x);
-    attach(next_y);
-    attach(xy_out);
-    xy_out.put("no point");
-    color_menu.attach(new Button(Point(0,0),0,0,"red",cb_red));
-    color_menu.attach(new Button(Point(0,0),0,0,"blue",cb_blue));
-    color_menu.attach(new Button(Point(0,0),0,0,"black",cb_black));
-    attach(color_menu);
-    color_menu.hide();
-    attach(menu_button);
-    style_menu.attach(new Button(Point(0,0),0,0,"solid",cb_solid));
-    style_menu.attach(new Button(Point(0,0),0,0,"dash",cb_dash));
-    style_menu.attach(new Button(Point(0,0),0,0,"dot",cb_dot));
-    attach(style_menu);
-    style_menu.hide();
-    attach(style_menu_button);
-    attach(lines);
+    checkerboard.push_back(new Button(Point(0,20), w/2, h/2-20, "", 
+    [](Address, Address pw) { reference_to<My_window>(pw).output(Board::a0); }));
+    checkerboard.push_back(new Button(Point(w/2,20), w/2, h/2-20, "", 
+    [](Address, Address pw) { reference_to<My_window>(pw).output(Board::a1); }));
+    checkerboard.push_back(new Button(Point(0,h/2), w/2, h/2, "", 
+    [](Address, Address pw) { reference_to<My_window>(pw).output(Board::b0); }));
+    checkerboard.push_back(new Button(Point(w/2,h/2), w/2, h/2, "", 
+    [](Address, Address pw) { reference_to<My_window>(pw).output(Board::b1); }));
+    for (int i=0; i<checkerboard.size(); i++)
+    	attach(checkerboard[i]);
 }
 
 //------------------------------------------------------------------------------
 
-void Lines_window::cb_quit(Address, Address pw)    // "the usual"
-{  
-       reference_to<Lines_window>(pw).quit();
+void My_window::cb_quit(Address, Address pw)    // "the usual"
+{
+       reference_to<My_window>(pw).quit();
 } 
 
 //------------------------------------------------------------------------------
 
-void Lines_window::quit()
+void My_window::quit()
 {
     hide();        // curious FLTK idiom for delete window
 }
 
 //------------------------------------------------------------------------------
 
-void Lines_window::cb_next(Address, Address pw)     // "the usual"
-{  
-       reference_to<Lines_window>(pw).next();
-} 
-
-//------------------------------------------------------------------------------
-
-void Lines_window::next()
+void My_window::output(Board b)
 {
-    int x = next_x.get_int();
-    int y = next_y.get_int();
-
-    lines.add(Point(x,y));
-
-    // update current position readout:
-    stringstream ss;
-    ss << '(' << x << ',' << y << ')';
-    xy_out.put(ss.str());
-
-      redraw();
+	ostringstream oss;
+    if (b == Board::a0) {
+    	oss << "(0,20)";
+    	out.put(oss.str());
+    }
+    if (b == Board::a1) {
+    	oss << "(" <<  x_max()/2 << ",20)";
+    	out.put(oss.str());
+    }
+    if (b == Board::b0) {
+    	oss << "(0," << y_max()/2 << ")";
+    	out.put(oss.str());
+    }
+    if (b == Board::b1) {
+    	oss << "(" << x_max()/2 << "," << y_max()/2 << ")";
+    	out.put(oss.str());
+    }
+    //redraw();
 }
-
-//------------------------------------------------------------------------------
-
-void Lines_window::cb_red(Address, Address pw)      // "the usual"
-{  
-    reference_to<Lines_window>(pw).red_pressed();
-} 
-
-//------------------------------------------------------------------------------
-
-void Lines_window::cb_blue(Address, Address pw)     // "the usual"
-{  
-    reference_to<Lines_window>(pw).blue_pressed();
-} 
-
-//------------------------------------------------------------------------------
-
-void Lines_window::cb_black(Address, Address pw)    // "the usual"
-{  
-    reference_to<Lines_window>(pw).black_pressed();
-} 
-
-//------------------------------------------------------------------------------
-
-void Lines_window::cb_menu(Address, Address pw)     // "the usual"
-{  
-    reference_to<Lines_window>(pw).menu_pressed();
-} 
-
-//------------------------------------------------------------------------------
-
-void Lines_window::cb_solid(Address, Address pw)     // "the usual"
-{  
-    reference_to<Lines_window>(pw).solid_pressed();
-} 
-
-//------------------------------------------------------------------------------
-
-void Lines_window::cb_dash(Address, Address pw)     // "the usual"
-{  
-    reference_to<Lines_window>(pw).dash_pressed();
-} 
-
-//------------------------------------------------------------------------------
-
-void Lines_window::cb_dot(Address, Address pw)     // "the usual"
-{  
-    reference_to<Lines_window>(pw).dot_pressed();
-} 
-
-//------------------------------------------------------------------------------
-
-void Lines_window::cb_style_menu(Address, Address pw)     // "the usual"
-{  
-    reference_to<Lines_window>(pw).style_menu_pressed();
-} 
 
 //------------------------------------------------------------------------------
 
 int main()
 try {
-    Lines_window win(Point(100,100),600,400,"lines");
+    My_window win(Point(100,100),900,600,"Exercises 1-2.");
     return gui_main();
 }
 catch(exception& e) {
--- code.h
+++ code.h
@@ -10,52 +10,21 @@
 
 //------------------------------------------------------------------------------
 
-struct Lines_window : Window {
-    Lines_window(Point xy, int w, int h, const string& title );
+struct My_window : Window {
+    My_window(Point xy, int w, int h, const string& title );
 private:
-    // Data:
-    Open_polyline lines;
-
+    enum Board { a0, a1, b0, b1 };
+ 
     // Widgets:
-    Button next_button;    // add (next_x,next_y) to lines
+    Out_box	out;
     Button quit_button;    // end program
-    In_box next_x;
-    In_box next_y;
-    Out_box xy_out;
-    Menu color_menu;
-    Menu style_menu;
-    Button style_menu_button;
-    Button menu_button;
+	Vector_ref<Button> checkerboard;
 
-
-    void change_color(Color c) { lines.set_color(c); } 
-    void hide_menu() { color_menu.hide(); menu_button.show(); } 
-    void hide_style_menu() { style_menu.hide(); style_menu_button.show(); } 
-
-    // actions invoked by callbacks:
-    void red_pressed() { change_color(Color::red); hide_menu(); }
-    void blue_pressed() { change_color(Color::blue); hide_menu(); } 
-    void black_pressed() { change_color(Color::black); hide_menu(); } 
-    void menu_pressed() { menu_button.hide(); color_menu.show(); }
-    void next();
+    void output(Board);
     void quit();
-	void solid_pressed() { lines.set_style(Line_style::solid); hide_style_menu(); }
-	void dash_pressed() { lines.set_style(Line_style::dash); hide_style_menu(); }
-	void dot_pressed() { lines.set_style(Line_style::dot); hide_style_menu(); }
-    void style_menu_pressed() { style_menu_button.hide(); style_menu.show(); }
 
     // callback functions:
-    static void cb_red(Address, Address);
-    static void cb_blue(Address, Address);
-    static void cb_black(Address, Address); 
-    static void cb_menu(Address, Address);
-    static void cb_next(Address, Address);
     static void cb_quit(Address, Address);
-    static void cb_style_menu(Address, Address);
-    static void cb_solid(Address, Address);
-    static void cb_dash(Address, Address);
-    static void cb_dot(Address, Address); 
-
 };
 
 //------------------------------------------------------------------------------
