3af654c861920656e7d8200c9859e7e6721d01b4
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2016-02-24 10:26:08 +0000

    Chapter 17.5 Try this: How do destructors (garbage collectors)

--- code.cpp
+++ code.cpp
@@ -1,114 +1,56 @@
-/*
- Chapter 16. Exercises 10.
-	10. Provide a program where you can choose among a set of functions (e.g., sin() and log()), provide parameters for those
-functions, and then graph them.
+/* 
+ * Chapter 17.5 "Try This"
+ * Write a little program using base classes and members where you define the
+ * constructors and destructors to output a line of information when they are
+ * called. Then, create a few objects and see how their constructors
+ * and destructors are called.
+ * c++ -o code code.cpp -std=c++11
+ */
+#include <iostream>
+#include <vector>
 
- clear && c++ -o code GUI/Simple_window.cpp GUI/Graph.cpp GUI/GUI.cpp GUI/Window.cpp code.cpp -lfltk -lfltk_images -std=c++11 && ./code
-*/
-
-#include "code.h"
-
-using namespace Graph_lib;
 using namespace std;
 
-//------------------------------------------------------------------------------
-//Constructor
-Graphics_window::Graphics_window(Point P, int width, int height, const string& lable):
-    Window (P,width,height,lable),
-    quit_button (Point(x_max()-80,0),80,30,"Quit", [](Address, Address pw) 
-            { reference_to<Graphics_window>(pw).quit(); } ),
-    functions_menu (Point(0,0),80,30,Menu::vertical,""),
-    r_min_box (Point(140,0),100,30,"r_min:"),
-    r_max_box (Point(300,0),100,30,"r_max:"),
-    x_scale_box (Point(140,35),100,30,"x_scale"),
-    y_scale_box (Point(300,35),100,30,"y_scale:"),
-    x (Axis::x, Point(width/8,height/2), width*6/8,0,"X"),
-    y (Axis::y, Point(width/2,height*7/8), height*6/8,0,"Y")
+double* init_d(double dd, string str)
 {
-    srand (time(NULL));
-    attach(quit_button);
-    functions_menu.attach(new Button(Point(0,0),0,0,"Sin()",[](Address, Address pw)
-                { reference_to<Graphics_window>(pw).f(sin); } ));
-    functions_menu.attach(new Button(Point(0,0),0,0,"Cos()",[](Address, Address pw)
-                { reference_to<Graphics_window>(pw).f(cos); } ));
-    functions_menu.attach(new Button(Point(0,0),0,0,"Log()",[](Address, Address pw)
-                { reference_to<Graphics_window>(pw).f(log); } ));
-    functions_menu.attach(new Button(Point(0,0),0,0,"Exp()",[](Address, Address pw)
-                { reference_to<Graphics_window>(pw).f(exp); } ));
-    functions_menu.attach(new Button(Point(0,0),0,0,"Acos()",[](Address, Address pw)
-                { reference_to<Graphics_window>(pw).f(acos); } ));
-    functions_menu.attach(new Button(Point(0,0),0,0,"Asin()",[](Address, Address pw)
-                { reference_to<Graphics_window>(pw).f(asin); } ));
-    functions_menu.attach(new Button(Point(0,0),0,0,"Atan()",[](Address, Address pw)
-                { reference_to<Graphics_window>(pw).f(atan); } ));
-
-    attach(functions_menu);
-    attach(r_min_box);
-    attach(r_max_box);
-    attach(x_scale_box);
-    attach(y_scale_box);
-    x.set_color(Color::red);
-    y.set_color(Color::red);
-    attach(x);
-    attach(y);
+    cout << str+" " << dd << " init_d initializator" << endl;
+    return new double(dd);
 }
 
-/////////////////////////////////////////////////////////////////////////////////////
-//  Random
- 
-int rand_int(int min, int max)
-{
-    return rand() % max + min;
-}
+class Parent {
+    public:
+        void date() { cout << *d << endl; } 
+        virtual ~Parent() { cout << "Parent " << *d <<  " DEstructor" << endl; delete d; }
+        Parent(double dd): d(init_d(dd,"Parent")) { cout << "Parent " << *d <<  " constructor" << endl; }
+    private:
+        double* d;
+};
 
-/////////////////////////////////////////////////////////////////////////////////////
-//  Get string tyoe value
-string  In_box::get_string()
+class Child : public Parent {
+    public:
+        void date() { cout << *r << endl; }
+        virtual ~Child() { cout << "Child " << *r <<  " DEstructor" << endl; delete r; }
+        Child(double dd): r(init_d(dd,"Child")), Parent(dd+1) { cout << "Child " << *r <<  " constructor" << endl; }
+    private:
+        double* r;
+};
+
+Parent* fct()
 {
-    Fl_Input& pi = reference_to<Fl_Input>(pw);
-    const char* p = pi.value();
+    Parent* p = new Child(7);
     return p;
 }
 
-/////////////////////////////////////////////////////////////////////////////////////
-//  Converting string to int
-int str_to_int(string str)
+int main ()
 {
-    stringstream ss;
-    ss << str;
-    int value = 0;
-    ss >> value;
-    return value;
+    cout << "Parent* q = fct();" << endl;
+    Parent* q = fct();
+    cout << "Parent k(3);" << endl;
+    Parent k(5);
+    cout << "Child f(2);" << endl;
+    Child f(2);
+    cout << "delete q;" << endl;
+    delete q;
+    cout << "return 0;" << endl;
+    return 0;
 }
-
-/////////////////////////////////////////////////////////////////////////////////////
-//  Draw the selected function
-void Graphics_window::f(Fct g)
-{
-    int r_min = str_to_int(r_min_box.get_string());
-    int r_max = str_to_int(r_max_box.get_string());
-    int x_scale = str_to_int(x_scale_box.get_string());
-    int y_scale = str_to_int(y_scale_box.get_string());
-    funcs.push_back(new Function(g,r_min,r_max,Point(x_max()/2,y_max()/2),200,x_scale,y_scale));
-    funcs[funcs.size()-1].set_color(rand_int(0,256));
-    attach(funcs[funcs.size()-1]);
-    redraw();
-}
-
-//------------------------------------------------------------------------------
-int main()
-try {
-    Graphics_window win(Point(100,100),800,600,"Chapter 16. The functions drawing.");
-	return gui_main();
-}
-catch(exception& e) {
-    cerr << "exception: " << e.what() << '\n';
-    return 1;
-}
-catch (...) {
-    cerr << "Some exception\n";
-    return 2;
-}
-
-//------------------------------------------------------------------------------
-
--- code.h
+++ code.h
@@ -8,23 +8,3 @@
 #include <list>
 #include <FL/Fl_Output.H>
 
-//------------------------------------------------------------------------------
-class Graphics_window : public Window {
-private:
-    Button quit_button;
-    Menu functions_menu;
-    In_box r_min_box;
-    In_box r_max_box;
-    In_box x_scale_box;
-    In_box y_scale_box;
-    Axis x;
-    Axis y;
-    Vector_ref<Function> funcs;
-
-public:
-    Graphics_window(Point, int, int, const string&);
-    void quit() { hide(); }
-    void f(Fct);
-};
-
-//------------------------------------------------------------------------------
