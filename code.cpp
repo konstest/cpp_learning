c2900be60152eeec71818ee610a87492794d8bbf
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2016-03-03 09:31:52 +0000

    Chapter 17. Exercises 8.

--- code.cpp
+++ code.cpp
@@ -1,35 +1,33 @@
 /* 
- * Chapter 17 Exercises 7 
- * Write a program that reads characters from cin into an array that you
- * allocate on the free store. Read individual characters until an exclamation
- * mark (!) is entered. Do not use a std::string. Do not worry about memory
- * exhaustion.
+ * Chapter 17 Exercises 8 
+ * Do exercise 7 again, but this time read into a std::string rather than to
+ * memory you put on the free store (string know show to use the free store for
+ * you).
  * c++ -o code code.cpp -std=c++11 && ./code
  */
 #include <iostream> 
 
 using namespace std;
 
-string* read_cin()
+string* read_cin(string* str)
 {
     char ch;
-    string* str;
     cout << "\tInput characters:\n";
     while(cin.get(ch)) {
         if (ch == '!') break;
-        *str->push_back(ch);
+        str->push_back(ch);
     }
     return str;
 }
 
 void print_all(string* s)
 {
-    cout << s << endl;
+    cout << "PRINT ALL characters:\n" << *s << endl;
 }
 
 int main() 
 { 
-    string* p = read_cin();
-    print_all(p);
+    string str;
+    print_all(read_cin(&str));
     return 0;
 }
