523c67c548b59ee86272e60e775705a88785954c
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2016-03-05 17:04:42 +0000

    Chapter 17. Exercises 9. Which way does the stack/free store grow?

--- code.cpp
+++ code.cpp
@@ -1,33 +1,73 @@
 /* 
- * Chapter 17 Exercises 8 
- * Do exercise 7 again, but this time read into a std::string rather than to
- * memory you put on the free store (string know show to use the free store for
- * you).
+ * Chapter 17 Exercises 9 
+ * Which way does the stack grow: up (toward higher addresses) or down (toward
+ * lower addresses)? Which way does the free store initially grow (that is, 
+ * before you use delete)? Write a program to determine the answers.
+ * Answers:
+ *      STACK grow to DOWN;
+ *      FREE stoew grow to UP.
  * c++ -o code code.cpp -std=c++11 && ./code
  */
 #include <iostream> 
+#include <vector> 
 
 using namespace std;
 
-string* read_cin(string* str)
+void func1()
 {
-    char ch;
-    cout << "\tInput characters:\n";
-    while(cin.get(ch)) {
-        if (ch == '!') break;
-        str->push_back(ch);
-    }
-    return str;
+    cout << "STACK STORE:" << endl;
+    int q = 203;
+    cout << "int[" << sizeof(q) << "] q[" << &q << "] = " << q << ";" << endl;
+    vector<int> e;
+    cout << "vector<int>[" << sizeof(e) << "] e[" << &e << "];\n";
+    cout << "FREE STORE:" << endl;
+    e.push_back(88);
+    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
+    e.push_back(8);
+    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
 }
 
-void print_all(string* s)
+void func2()
 {
-    cout << "PRINT ALL characters:\n" << *s << endl;
+    cout << "STACK STORE:" << endl;
+    vector<int> e;
+    cout << "vector<int>[" << sizeof(e) << "] e[" << &e << "];\n";
+    short q = 233;
+    cout << "short[" << sizeof(q) << "] q[" << &q << "] = " << q << ";" << endl;
+    char C[15] = "192.168.0.7";
+    cout << "char[" << sizeof(C) << "] C[" << &C << "] = " << C << ";" << endl;
+    cout << "FREE STORE:" << endl;
+    e.push_back(88);
+    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
+}
+
+void func3()
+{
+    cout << "STACK STORE:" << endl;
+    double q = 2;
+    cout << "double[" << sizeof(q) << "] q[" << &q << "] = " << q << ";" << endl;
+    vector<int> e;
+    cout << "vector<int>[" << sizeof(e) << "] e[" << &e << "];\n";
+    cout << "FREE STORE:" << endl;
+    e.push_back(18);
+    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
+    e.push_back(19);
+    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
+    e.push_back(20);
+    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
 }
 
 int main() 
 { 
-    string str;
-    print_all(read_cin(&str));
+    cout << "\tfunc1(): \n";
+    func1();
+    cout << "\tfunc2(): \n";
+    func2();
+    cout << "\tfunc3(): \n";
+    func3();
+    cout << "\n\tAnswers:\n";
+    cout << "STACK grow to DOWN;\n";
+    cout << "FREE stoew grow to UP.\n";
     return 0;
 }
+
