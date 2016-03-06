9567bf913469a8e8eb27f2d3be49d922a66744de
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2016-03-06 06:42:04 +0000

    Chapter 17. Exercises 10.

--- code.cpp
+++ code.cpp
@@ -1,73 +1,52 @@
 /* 
- * Chapter 17 Exercises 9 
- * Which way does the stack grow: up (toward higher addresses) or down (toward
- * lower addresses)? Which way does the free store initially grow (that is, 
- * before you use delete)? Write a program to determine the answers.
+ * Chapter 17 Exercises 10 
+ * Look at your solution of exercise 7. Is there any way that input could get
+ * the array to overflow; that is, is there any way you could enter more
+ * characters than you allocated space for (a serious error)? Does anything
+ * reasonable happen if you try to enter more characters than you allocated?
  * Answers:
- *      STACK grow to DOWN;
- *      FREE stoew grow to UP.
+ *      Array overflow: input a characters of escape
+ *          sequence(ctrl+1/2/3/4/etc, arrow down/up/left/right).
+ *      When is we enter more characters than is allocated is nothing happen.
  * c++ -o code code.cpp -std=c++11 && ./code
  */
 #include <iostream> 
-#include <vector> 
 
 using namespace std;
 
-void func1()
+char* read_cin()
 {
-    cout << "STACK STORE:" << endl;
-    int q = 203;
-    cout << "int[" << sizeof(q) << "] q[" << &q << "] = " << q << ";" << endl;
-    vector<int> e;
-    cout << "vector<int>[" << sizeof(e) << "] e[" << &e << "];\n";
-    cout << "FREE STORE:" << endl;
-    e.push_back(88);
-    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
-    e.push_back(8);
-    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
+    char ch;
+    int k = 0, i = 0;
+    char* ptr = new char;
+    cout << "\tInput characters:\n";
+    while(cin.get(ch)) {
+        if (ch == '!') {
+            ptr[k] = '\0';
+            break;
+        }
+        k++;
+        char* tmp_ptr = new char[k];
+        for (i=0; i<k-1; i++)
+            tmp_ptr[i] = ptr[i];
+        tmp_ptr[i] = ch;
+        delete[] ptr;
+        ptr = tmp_ptr;
+    }
+    return ptr;
 }
 
-void func2()
+void print_all(const char* s)
 {
-    cout << "STACK STORE:" << endl;
-    vector<int> e;
-    cout << "vector<int>[" << sizeof(e) << "] e[" << &e << "];\n";
-    short q = 233;
-    cout << "short[" << sizeof(q) << "] q[" << &q << "] = " << q << ";" << endl;
-    char C[15] = "192.168.0.7";
-    cout << "char[" << sizeof(C) << "] C[" << &C << "] = " << C << ";" << endl;
-    cout << "FREE STORE:" << endl;
-    e.push_back(88);
-    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
-}
-
-void func3()
-{
-    cout << "STACK STORE:" << endl;
-    double q = 2;
-    cout << "double[" << sizeof(q) << "] q[" << &q << "] = " << q << ";" << endl;
-    vector<int> e;
-    cout << "vector<int>[" << sizeof(e) << "] e[" << &e << "];\n";
-    cout << "FREE STORE:" << endl;
-    e.push_back(18);
-    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
-    e.push_back(19);
-    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
-    e.push_back(20);
-    cout << "e.push_back(" << e[e.size()-1] << ")[" << &e[e.size()-1] << "];" << endl;
+    cout << "\tPRINT ALL characters:\n";
+    for(int i=0; s[i] != '\0'; i++)
+        cout << s[i];
+    cout << endl;
 }
 
 int main() 
 { 
-    cout << "\tfunc1(): \n";
-    func1();
-    cout << "\tfunc2(): \n";
-    func2();
-    cout << "\tfunc3(): \n";
-    func3();
-    cout << "\n\tAnswers:\n";
-    cout << "STACK grow to DOWN;\n";
-    cout << "FREE stoew grow to UP.\n";
+    char* p = read_cin();
+    print_all(p);
     return 0;
 }
-
