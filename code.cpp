fc14f393dda20934adc86b1c834eda3afac0fc8b
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2016-03-21 10:01:41 +0000

    Chapter 18. Exercise 7.

--- code.cpp
+++ code.cpp
@@ -1,28 +1,31 @@
 /* 
  * Chapter 18. Exercises 1,2,3.
- * 5.
- * Write a function, string cat_dot(const string& s1, const string& s2), that
- * concatenates two strings with a dot inbetween. For example, cat_dot("Niels",
- * "Bohr") will return a string containing Niels.Bohr.
- * 6.
- * Modify cat_dot() from * the previous exercise to take a string to be used 
- * as the separator (rather * than dot) as its thirdargument.
+ * 7.
+ * Write versions of the cat_dot()s from the previous exercises to take C-style
+ * strings as arguments and return a free-store-allocated C-style string as the
+ * result. Do not use standard library functions or types in the
+ * implementation. Test thesefunctions with several strings. Be sure to free
+ * (using delete) all the memory you allocated from free store (using
+ * new).Compare the effort involved in this exercise with the effort involved
+ * for exercises 5 and 6.
  */
 
 #include "code.h"
 
 using namespace std;
 
-string cat_dot(const string& s1, const string& sep, const string& s2)
+string* cat_dot(const string& s1, const string& sep, const string& s2)
 {
-    return s1+sep+s2;
+    return new string{s1+sep+s2};
 }
 
 int main()
 {
-    cout << "Chapter 18. Exercise 5.\n" << endl;
+    cout << "Chapter 18. Exercise 7.\n";
     string s1 = "Genri", s2 = "Ford";
-    cout << cat_dot(s1," ",s2) << endl;
+    string* sss = cat_dot(s1," ",s2);
+    cout << *sss << endl;
+    delete sss;
 
     return 0;
 }
