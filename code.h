181294e303a77029145d06818e2862f320732c07
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2016-05-13 09:12:50 +0000

    Chapter 19. Exercise 3.

--- code.h
+++ code.h
@@ -4,28 +4,23 @@
 
 #include "std_lib_facilities.h"
 
-struct Type1 {
-    int a;
-    double b;
-    Type1(): a(0), b(0.0) { }
-    Type1& operator+=(const Type1& t) {
-        this->a += t.a;
-        this->b += t.b;
-        return *this;
-    }
-    Type1 operator+(const Type1& rt);
+template<typename A, typename B>
+class Pair {
+public:
+    Pair(const A& a, const B& b): v1(a), v2(b) { }
+    A v1;
+    B v2;
 };
 
-template<typename T>
-double operator+(T& l, const Type1& r);             //for excercise 2.
+template<typename A, typename B>
+ostream& operator<<(ostream&, const Pair<A,B>&);
 
-template<typename T>
-double operator*(Type1& l, const T& r);             //for excercise 2.
 
-ostream& operator<<(ostream& os, const Type1& t);   //overload output
+struct Type {
+    int a;
+    double b;
+    Type(): a(0), b(0.0) { }
+    Type(int aa, double bb): a(aa), b(bb) { }
+};
 
-template<typename T>
-void f(vector<T>& v1, const vector<T>& v2);         //for excercise 1.
-
-template<typename T, typename U>
-double mul(vector<T> vt, vector<U> vu);             //for excercise 2.
+ostream& operator<<(ostream& os, const Type& t);   //overload output for Type
