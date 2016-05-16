077f88ee81d191f1da5534786fb9c1ef91824fef
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2016-05-16 09:22:35 +0000

    Chapter 19. Exercise 4.

--- code.cpp
+++ code.cpp
@@ -1,55 +1,193 @@
 /* 
- * Chapter 19. Exercise 3.
- * Write a template class Pair that can hold a pair of values of any type. Use
- * this to implement a simple symbol table likethe one we used in the
- * calculator (§7.8).
+ * Chapter 19. Exercise 4.
+ * Modify class Link from §17.9.3 to be a template with the type of value as
+ * the template argument. Then redo exercise 13from Chapter 17 with Link<God>.
+ * clear; g++ -o code code.cpp -std=c++0x && ./code
  */
 
-#include "std_lib_facilities.h"
+#include <iostream> 
 #include "code.h"
 
-//------------------------------------------------------------------------------
-//  Overloading output for Pair
-template<typename A, typename B>
-ostream& operator<<(ostream& os, const Pair<A,B>& p) 
+using namespace std;
+
+template<typename T>
+Link<T>* Link<T>::insert(Link<T>* n) // insert n before this object;
 {
-    os << p.v1 << ":\t" << p.v2;
-    return os;
+    if (n==nullptr) return this;
+    if (this==nullptr) return n;
+    n->succ = this;
+    if (prev) prev->succ = n;
+    n->prev = prev;
+    prev = n;
+    return n;
 }
 
-//------------------------------------------------------------------------------
-// Overload output for Type
-ostream& operator<<(ostream& os, const Type& t)
+template<typename T>
+Link<T>* Link<T>::add(Link<T>* n) // add n after this object;
 {
-    os << "a(" << t.a << "), b(" << t.b << ")";
-    return os;
+    if (n==nullptr) return this;
+    if (this==nullptr) return n;
+    n->prev = this;
+    if (succ) succ->prev = n;
+    n->succ = succ;
+    succ = n;
+    return n;
 }
- 
-//------------------------------------------------------------------------------
-int main()
-{
-    vector<Pair<string,double>> table;
 
-    Pair<string,double> pi("pi",3.1415926535);
-    table.push_back(pi);
-    Pair<string,double> e("e",2.7182818284);
-    table.push_back(e);
+template<typename T>
+// places new element in its correct lexicographical position.
+Link<T>* Link<T>::add_ordered(Link<T>* n) 
+{
+    if (n==nullptr) return this;
+    // if *n already to other list, then:
+    if (n->succ)
+        n->succ->prev = n->prev;
+    if (n->prev)
+        n->prev->succ = n->succ;
+    n->succ = nullptr;
+    n->prev = nullptr;
     
-    for (int i=0; i<table.size(); i++)
-        cout << table[i] << endl;
+    if (this==nullptr) return n;
 
-    Pair<string,Type> test0("Type0",Type());
-    test0.v2.a = 5;
-    test0.v2.b = 7.3;
-    vector<Pair<string,Type>> table2;
-    table2.push_back(test0);
-    Pair<string,Type> test1("Type1",Type(123,9.0001));
-    table2.push_back(test1);
-
-    for (int i=0; i<table2.size(); i++)
-        cout << table2[i] << endl;
-
-    return 0;
+    Link<T>* p = this;
+    while (p->prev)
+        p = p->prev;
+    Link<T>* head = p;
+    while (p) {
+        if ( p->succ ) {
+            if ( p->value.name < n->value.name && n->value.name < p->succ->value.name ) {
+                p->add(n);
+                return head;
+            }
+        }
+        else {
+            p->add(n);
+            return head;
+        }
+        p = p->succ;
+    }
 }
 
-//------------------------------------------------------------------------------
+template<typename T>
+Link<T>* Link<T>::find(const string& s)
+{
+    Link<T>* p = this;
+    while (p) {
+        if (p->value.name == s) return p;
+        p = next();
+    }
+    p = this;
+    while (p) {
+        if (p->value.name == s) return p;
+        p = previous();
+    }
+    return nullptr;
+}
+
+template<typename T>
+Link<T>* Link<T>::erase()
+{
+    if (prev) prev->succ = succ;
+    if (succ) succ->prev = prev;
+    if (succ)
+        return succ;
+    else if (prev)
+        return prev;
+    delete this;
+    return nullptr;
+}
+
+template<typename T>
+void print_all(Link<T>* p)
+{
+    if (p) {
+        cout << "{\n";
+        while (p) {
+            cout << "name: " << p->value.name
+                << ", mythology: " << p->value.mythology
+                << ", vehicle: " << p->value.vehicle
+                << ", weapon: " << p->value.weapon;
+            if (p=p->next()) cout << endl;
+        }
+        cout << "\n}\n";
+    }
+}
+
+template<typename T>
+Link<T>* new_mythology_list(Link<T>* link, const string& myth)
+{
+    Link<T>* p = link, *tmp = nullptr, *new_mythology = nullptr;
+    while (p) {
+        if ( p->value.mythology == myth ) {
+            tmp = p;
+            p = p->next();
+            new_mythology = new_mythology->add_ordered(tmp);
+        }
+        else
+            p = p->next();
+    }
+    return new_mythology;
+}
+
+int main() 
+{ 
+    try {
+        Link<God> *norse_gods = nullptr, *greek_gods = nullptr, *egypt_gods = nullptr;
+        Link<God> *gods = new Link<God>{God{"Thor","Norse","","Hammer"}};
+        gods = gods->insert(new Link<God>{God{"Odin", "Norse", "Eight-legged \
+    flying horse called Sleipner", "Spear called Gungnir"}});
+        gods = gods->insert(new Link<God>{God{"Zeus", "Greek", "", "lightning"}});
+        gods = gods->insert(new Link<God>{God{"Taurt","Egypt","","Patroness of women and children"}});
+        gods = gods->insert(new Link<God>{God{"Freia","Norse","Rides a chariot \
+    pulled by two cats",""}});
+        gods = gods->insert(new Link<God>{God{"Hera","Greek","Chariot drawn by peacocks",""}});
+        gods = gods->insert(new Link<God>{God{"Athena","Greek","",""}});
+        gods = gods->insert(new Link<God>{God{"Osiris","Egypt","","God of rebirth"}});
+        gods = gods->insert(new Link<God>{God{"Mars","Greek","","God of war"}});
+        gods = gods->insert(new Link<God>{God{"Poseidon","Greek","The chariot \
+    pulled by sea horses","Trident"}});
+        gods = gods->insert(new Link<God>{God{"Anubis","Egypt","","Conductor dead in the \
+    underworld kingdom, oberegatel cemeteries and mummies"}});
+
+        cout << "All gods:\n";
+        print_all(gods);
+        
+        cout << endl;
+        norse_gods = new_mythology_list(gods,"Norse");
+        greek_gods = new_mythology_list(gods,"Greek");
+        egypt_gods = new_mythology_list(gods,"Egypt");
+
+        cout << "Norse gods:\n";
+        print_all(norse_gods);
+
+        cout << "Greek gods:\n";
+        print_all(greek_gods);
+
+        cout << "Egypt gods:\n";
+        print_all(egypt_gods);
+
+        //Test on error
+        cout << endl << "Test on error\n";
+        Link<int> *test = new Link<int>(1);
+        test = test->insert(new Link<int>(2));
+        test = test->insert(new Link<int>(3));
+        test = test->insert(new Link<int>(4));
+//        test = test->add_ordered(new Link<int>(5));     //error: request for member ‘name’ in ‘p->Link<int>::succ->Link<int>::value’, 
+                                                        //which is of non-class type ‘int’
+                                                        //error: request for member ‘name’ in ‘n->Link<int>::value’, which is
+                                                        //of non-class type ‘int’
+        while (test) {
+            cout << test->value << endl;
+            test = test->next();
+        }
+
+        Link<Type1> *test1 = new Link<Type1>(Type1());
+        test1 = test1->insert(new Link<Type1>(Type1()));
+//        test1 = test1->add_ordered(new Link<Type1>(Type1()));   //error: ‘struct Type1’ has no member named ‘name’
+        return 0;
+    }
+    catch (...) {
+        error("Catch error!");
+        return 1;
+    }
+}
--- code.h
+++ code.h
@@ -1,26 +1,66 @@
 /* 
- * Headres file
+ * Заглолвочный файл
  */
 
 #include "std_lib_facilities.h"
+#include <list>
 
-template<typename A, typename B>
-class Pair {
-public:
-    Pair(const A& a, const B& b): v1(a), v2(b) { }
-    A v1;
-    B v2;
+struct God {
+    string name, mythology, vehicle, weapon;
+    God(string n, string m, string v, string w)
+        :name(n), mythology(m), vehicle(v), weapon(w) { }
 };
 
-template<typename A, typename B>
-ostream& operator<<(ostream&, const Pair<A,B>&);
 
 
-struct Type {
+template<typename T>
+class Link {
+public:
+    T value;
+    Link(const T& v, Link* p = nullptr, Link* s = nullptr)
+        : value{v}, prev{p}, succ{s} { }
+    Link* insert(Link* n);          // insert n before this object
+    Link* add(Link* n);             // insert n after this object
+    Link* add_ordered(Link*);       // places new element in its correct lexicographical position.
+    Link* erase();                  // remove this object from list
+    Link* find(const string& s);    // find s in list
+    const Link* find(const string& s) const;    // find s in const list (see §18.5.1)
+//    Link* advance(int n) const;     // move n positions in list
+    Link* next() const { return succ; }
+    Link* previous() const { return prev; }
+private:
+    Link* prev;
+    Link* succ;
+};
+
+
+
+
+
+
+struct Type1 {
     int a;
     double b;
-    Type(): a(0), b(0.0) { }
-    Type(int aa, double bb): a(aa), b(bb) { }
+    Type1(): a(0), b(0.0) { }
+    Type1& operator+=(const Type1& t) {
+        this->a += t.a;
+        this->b += t.b;
+        return *this;
+    }
+    Type1 operator+(const Type1& rt);
 };
 
-ostream& operator<<(ostream& os, const Type& t);   //overload output for Type
+template<typename T>
+double operator+(T& l, const Type1& r);             //for excercise 2.
+
+template<typename T>
+double operator*(Type1& l, const T& r);             //for excercise 2.
+
+ostream& operator<<(ostream& os, const Type1& t);   //overload output
+
+template<typename T>
+void f(vector<T>& v1, const vector<T>& v2);         //for excercise 1.
+
+template<typename T, typename U>
+double mul(vector<T> vt, vector<U> vu);             //for excercise 2.
+
