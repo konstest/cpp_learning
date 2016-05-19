91d319563c5dc9af2fe33e6b69ee15f049946ee3
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2016-05-19 05:20:12 +0000

    Chapter 19. Exercise 5.

--- code.cpp
+++ code.cpp
@@ -1,7 +1,8 @@
 /* 
- * Chapter 19. Exercise 4.
- * Modify class Link from §17.9.3 to be a template with the type of value as
- * the template argument. Then redo exercise 13from Chapter 17 with Link<God>.
+ * Chapter 19. Exercise 5.
+ * Define a class Int having a single member of class int. Define constructors,
+ * assignment, and operators +, -, *, / for it.Test it, and improve its design
+ * as needed (e.g., define operators << and >> for convenient I/O).
  * clear; g++ -o code code.cpp -std=c++0x && ./code
  */
 
@@ -10,184 +11,74 @@
 
 using namespace std;
 
-template<typename T>
-Link<T>* Link<T>::insert(Link<T>* n) // insert n before this object;
+//-----------------------------------------------------------------------------
+//  Overload math operators
+Int& Int::operator-=(const Int& rhs)
 {
-    if (n==nullptr) return this;
-    if (this==nullptr) return n;
-    n->succ = this;
-    if (prev) prev->succ = n;
-    n->prev = prev;
-    prev = n;
-    return n;
+    i -= rhs.get();
+    return *this;
 }
 
-template<typename T>
-Link<T>* Link<T>::add(Link<T>* n) // add n after this object;
+Int& Int::operator+=(const Int& rhs)
 {
-    if (n==nullptr) return this;
-    if (this==nullptr) return n;
-    n->prev = this;
-    if (succ) succ->prev = n;
-    n->succ = succ;
-    succ = n;
-    return n;
+    i += rhs.get();
+    return *this;
 }
 
-template<typename T>
-// places new element in its correct lexicographical position.
-Link<T>* Link<T>::add_ordered(Link<T>* n) 
+Int& Int::operator*=(const Int& rhs)
 {
-    if (n==nullptr) return this;
-    // if *n already to other list, then:
-    if (n->succ)
-        n->succ->prev = n->prev;
-    if (n->prev)
-        n->prev->succ = n->succ;
-    n->succ = nullptr;
-    n->prev = nullptr;
-    
-    if (this==nullptr) return n;
-
-    Link<T>* p = this;
-    while (p->prev)
-        p = p->prev;
-    Link<T>* head = p;
-    while (p) {
-        if ( p->succ ) {
-            if ( p->value.name < n->value.name && n->value.name < p->succ->value.name ) {
-                p->add(n);
-                return head;
-            }
-        }
-        else {
-            p->add(n);
-            return head;
-        }
-        p = p->succ;
-    }
+    i *= rhs.get();
+    return *this;
 }
 
-template<typename T>
-Link<T>* Link<T>::find(const string& s)
+Int& Int::operator/=(const Int& rhs)
 {
-    Link<T>* p = this;
-    while (p) {
-        if (p->value.name == s) return p;
-        p = next();
-    }
-    p = this;
-    while (p) {
-        if (p->value.name == s) return p;
-        p = previous();
-    }
-    return nullptr;
+    i /= rhs.get();
+    return *this;
 }
 
-template<typename T>
-Link<T>* Link<T>::erase()
+Int operator-(Int lhs, const Int& rhs)
 {
-    if (prev) prev->succ = succ;
-    if (succ) succ->prev = prev;
-    if (succ)
-        return succ;
-    else if (prev)
-        return prev;
-    delete this;
-    return nullptr;
+    return lhs -= rhs;
 }
 
-template<typename T>
-void print_all(Link<T>* p)
+Int operator+(Int lhs, const Int& rhs)
 {
-    if (p) {
-        cout << "{\n";
-        while (p) {
-            cout << "name: " << p->value.name
-                << ", mythology: " << p->value.mythology
-                << ", vehicle: " << p->value.vehicle
-                << ", weapon: " << p->value.weapon;
-            if (p=p->next()) cout << endl;
-        }
-        cout << "\n}\n";
-    }
+    return lhs += rhs;
 }
 
-template<typename T>
-Link<T>* new_mythology_list(Link<T>* link, const string& myth)
+Int operator*(Int lhs, const Int& rhs)
 {
-    Link<T>* p = link, *tmp = nullptr, *new_mythology = nullptr;
-    while (p) {
-        if ( p->value.mythology == myth ) {
-            tmp = p;
-            p = p->next();
-            new_mythology = new_mythology->add_ordered(tmp);
-        }
-        else
-            p = p->next();
-    }
-    return new_mythology;
+    return lhs *= rhs;
 }
 
+Int operator/(Int lhs, const Int& rhs)
+{
+    return lhs /= rhs;
+}
+
+// overload output
+istream& operator>>(istream& is, Int& t)
+{
+    is >> t.set();
+    return is;
+}
+
+//-----------------------------------------------------------------------------
 int main() 
 { 
     try {
-        Link<God> *norse_gods = nullptr, *greek_gods = nullptr, *egypt_gods = nullptr;
-        Link<God> *gods = new Link<God>{God{"Thor","Norse","","Hammer"}};
-        gods = gods->insert(new Link<God>{God{"Odin", "Norse", "Eight-legged \
-    flying horse called Sleipner", "Spear called Gungnir"}});
-        gods = gods->insert(new Link<God>{God{"Zeus", "Greek", "", "lightning"}});
-        gods = gods->insert(new Link<God>{God{"Taurt","Egypt","","Patroness of women and children"}});
-        gods = gods->insert(new Link<God>{God{"Freia","Norse","Rides a chariot \
-    pulled by two cats",""}});
-        gods = gods->insert(new Link<God>{God{"Hera","Greek","Chariot drawn by peacocks",""}});
-        gods = gods->insert(new Link<God>{God{"Athena","Greek","",""}});
-        gods = gods->insert(new Link<God>{God{"Osiris","Egypt","","God of rebirth"}});
-        gods = gods->insert(new Link<God>{God{"Mars","Greek","","God of war"}});
-        gods = gods->insert(new Link<God>{God{"Poseidon","Greek","The chariot \
-    pulled by sea horses","Trident"}});
-        gods = gods->insert(new Link<God>{God{"Anubis","Egypt","","Conductor dead in the \
-    underworld kingdom, oberegatel cemeteries and mummies"}});
-
-        cout << "All gods:\n";
-        print_all(gods);
-        
-        cout << endl;
-        norse_gods = new_mythology_list(gods,"Norse");
-        greek_gods = new_mythology_list(gods,"Greek");
-        egypt_gods = new_mythology_list(gods,"Egypt");
-
-        cout << "Norse gods:\n";
-        print_all(norse_gods);
-
-        cout << "Greek gods:\n";
-        print_all(greek_gods);
-
-        cout << "Egypt gods:\n";
-        print_all(egypt_gods);
-
-        //Test on error
-        cout << endl << "Test on error\n";
-        Link<int> *test = new Link<int>(1);
-        test = test->insert(new Link<int>(2));
-        test = test->insert(new Link<int>(3));
-        test = test->insert(new Link<int>(4));
-//        test = test->add_ordered(new Link<int>(5));     //error: request for member ‘name’ in ‘p->Link<int>::succ->Link<int>::value’, 
-                                                        //which is of non-class type ‘int’
-                                                        //error: request for member ‘name’ in ‘n->Link<int>::value’, which is
-                                                        //of non-class type ‘int’
-        while (test) {
-            cout << test->value << endl;
-            test = test->next();
-        }
-
-        Link<Type1> *test1 = new Link<Type1>(Type1());
-        test1 = test1->insert(new Link<Type1>(Type1()));
-//        test1 = test1->add_ordered(new Link<Type1>(Type1()));   //error: ‘struct Type1’ has no member named ‘name’
+        Int h;
+        cout << "Input Int h: ";
+        cin >> h;
+        cout << "h = h / 3.2;\n";
+        h = h / 3.2;
+        cout << h << endl;
         return 0;
     }
     catch (...) {
-        error("Catch error!");
+        cerr << "Error occurred :(\n";
         return 1;
     }
 }
+//-----------------------------------------------------------------------------
--- code.h
+++ code.h
@@ -5,36 +5,27 @@
 #include "std_lib_facilities.h"
 #include <list>
 
-struct God {
-    string name, mythology, vehicle, weapon;
-    God(string n, string m, string v, string w)
-        :name(n), mythology(m), vehicle(v), weapon(w) { }
+class Int {
+    private:
+        int i;
+    public:
+        Int(): i(0) {}
+        Int(int k): i(k) {}
+        int     get() const { return i; }
+        void    set(int k) { i = k; }
+        int&    set() { return i; }
+        Int&    operator+=(const Int&);
+        Int&    operator-=(const Int&);
+        Int&    operator*=(const Int&);
+        Int&    operator/=(const Int&);
 };
 
-
-
-template<typename T>
-class Link {
-public:
-    T value;
-    Link(const T& v, Link* p = nullptr, Link* s = nullptr)
-        : value{v}, prev{p}, succ{s} { }
-    Link* insert(Link* n);          // insert n before this object
-    Link* add(Link* n);             // insert n after this object
-    Link* add_ordered(Link*);       // places new element in its correct lexicographical position.
-    Link* erase();                  // remove this object from list
-    Link* find(const string& s);    // find s in list
-    const Link* find(const string& s) const;    // find s in const list (see §18.5.1)
-//    Link* advance(int n) const;     // move n positions in list
-    Link* next() const { return succ; }
-    Link* previous() const { return prev; }
-private:
-    Link* prev;
-    Link* succ;
-};
-
-
-
+Int operator+(Int lhs, const Int& rhs);
+Int operator-(Int lhs, const Int& rhs);
+Int operator*(Int lhs, const Int& rhs);
+Int operator/(Int lhs, const Int& rhs);
+ostream& operator<<(ostream& os, const Int& t) { return os << t.get(); }
+istream& operator>>(istream& is, Int& t);
 
 
 
