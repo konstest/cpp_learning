28308659cac758f76d533c76a08bfd703890c0fc
konstantine <konstantine@71603f7b-e006-486f-adbe-bf2b547405aa>
2016-05-20 10:53:14 +0000

    Chapter 19. Exercise 7.

--- code.h
+++ code.h
@@ -15,6 +15,7 @@ class Number {
         T  get() const { return i; }
         void    set(T k) { i = k; }
         T&    set() { return i; }
+        double  return_to_double() { return i; };
         Number&    operator+=(const Number& rhs) { i += rhs.get(); return *this; }
         Number&    operator-=(const Number& rhs) { i -= rhs.get(); return *this; }
         Number&    operator*=(const Number& rhs) { i *= rhs.get(); return *this; }
@@ -23,19 +24,19 @@ class Number {
 };
 
 template<typename T, typename A> Number<T> operator+(Number<T> lhs, const A& rhs) { return lhs += rhs; }
-template<typename T, typename A> Number<T> operator+(A lhs, Number<T> rhs) { return lhs += rhs.get(); }
+template<typename T, typename A> A operator+(A lhs, Number<T> rhs) { return lhs += rhs.get(); }
 template<typename T, typename A> Number<T> operator+(Number<T> lhs, const Number<A>& rhs) { return lhs.set() += rhs.get(); } 
 template<typename T, typename A> Number<T> operator-(Number<T> lhs, const A& rhs) { return lhs -= rhs; } 
-template<typename T, typename A> Number<T> operator-(A lhs, Number<T> rhs) { return lhs -= rhs.get(); }
+template<typename T, typename A> A operator-(A lhs, Number<T> rhs) { return lhs -= rhs.get(); }
 template<typename T, typename A> Number<T> operator-(Number<T> lhs, const Number<A>& rhs) { return lhs.set() -= rhs.get(); } 
 template<typename T, typename A> Number<T> operator*(Number<T> lhs, const A& rhs) { return lhs *= rhs; }
-template<typename T, typename A> Number<T> operator*(A lhs, Number<T> rhs) { return lhs *= rhs.get(); }
+template<typename T, typename A> A operator*(A lhs, Number<T> rhs) { return lhs *= rhs.get(); }
 template<typename T, typename A> Number<T> operator*(Number<T> lhs, const Number<A>& rhs) { return lhs.set() *= rhs.get(); } 
 template<typename T, typename A> Number<T> operator/(Number<T> lhs, const A& rhs) { return lhs /= rhs; }
-template<typename T, typename A> Number<T> operator/(A lhs, Number<T> rhs) { return lhs /= rhs.get(); }
+template<typename T, typename A> A operator/(A lhs, Number<T> rhs) { return lhs /= rhs.get(); }
 template<typename T, typename A> Number<T> operator/(Number<T> lhs, const Number<A>& rhs) { return lhs.set() /= rhs.get(); } 
 template<typename T, typename A> Number<T> operator%(Number<T> lhs, const A& rhs) { return lhs %= rhs; }
-template<typename T, typename A> Number<T> operator%(A lhs, Number<T> rhs) { return lhs += (int)rhs.get(); }
+template<typename T, typename A> A operator%(A lhs, Number<T> rhs) { return lhs += (int)rhs.get(); }
 template<typename T, typename A> Number<T> operator%(Number<T> lhs, const Number<A>& rhs)
 {
     return lhs.set() = (int)lhs.set() % (int)rhs.get();
@@ -43,30 +44,4 @@ template<typename T, typename A> Number<T> operator%(Number<T> lhs, const Number
 
 template<typename T> ostream& operator<<(ostream& os, const Number<T>& t) { return os << t.get(); }
 template<typename T> istream& operator>>(istream& is, Number<T>& t);
-
-
-struct Type1 {
-    int a;
-    double b;
-    Type1(): a(0), b(0.0) { }
-    Type1& operator+=(const Type1& t) {
-        this->a += t.a;
-        this->b += t.b;
-        return *this;
-    }
-};
-
-template<typename T>
-double operator+(T& l, const Type1& r);             //for excercise 2.
-
-template<typename T>
-double operator*(Type1& l, const T& r);             //for excercise 2.
-
-ostream& operator<<(ostream& os, const Type1& t);   //overload output
-
-template<typename T>
-void f(vector<T>& v1, const vector<T>& v2);         //for excercise 1.
-
-template<typename T, typename U>
-double mul(vector<T> vt, vector<U> vu);             //for excercise 2.
 
